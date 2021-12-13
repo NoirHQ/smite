f4d71656a048280f2abd41a780357b15c3d07c58
Jeeyong Um <conr2d@gmail.com>
2021-12-13 18:00:47 +0900

    feat: update appbase

--- cmd/noird/main.cpp
+++ cmd/noird/main.cpp
@@ -5,10 +5,18 @@
 using appbase::app;
 
 int main(int argc, char** argv) {
+  app().cli().name("noird");
+  app().cli().description("NOIR Protocol App");
+  std::filesystem::path home_dir;
+  if (auto arg = std::getenv("HOME")) {
+    home_dir = arg;
+  }
+  app().set_home_dir(home_dir / ".noir");
+  app().set_config_file("app.toml");
   app().register_plugin<noir::tendermint>();
   if (!app().initialize<noir::tendermint>(argc, argv)) {
-    const auto& opts = app().get_options();
-    if(opts.count("help") || opts.count("version") || opts.count("full-version") || opts.count("print-default-config")) {
+    const auto& opts = app().cli();
+    if (opts.count("--help") || opts.count("--print-default-config")) {
       return 0;
     }
     return 1;
