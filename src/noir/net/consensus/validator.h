9bc35e9ba4e38601f423eef9e2af789b71bfc664
nonvis@gmail.com <nonvis@gmail.com>
2021-12-29 16:59:42 +0900

    Start implementing proposal selection process

--- src/noir/net/consensus/validator.h
+++ src/noir/net/consensus/validator.h
@@ -13,7 +13,73 @@ struct validator {
 struct validator_set {
   std::vector<validator> validators;
   validator proposer;
-  int64_t total_voting_power;
+  int64_t total_voting_power = 0;
+
+  void update_with_change_set() {
+// UpdateWithChangeSet attempts to update the validator set with 'changes'.
+// It performs the following steps:
+// - validates the changes making sure there are no duplicates and splits them in updates and deletes
+// - verifies that applying the changes will not result in errors
+// - computes the total voting power BEFORE removals to ensure that in the next steps the priorities
+//   across old and newly added validators are fair
+// - computes the priorities of new validators against the final set
+// - applies the updates against the validator set
+// - applies the removals against the validator set
+// - performs scaling and centering of priority values
+// If an error is detected during verification steps, it is returned and the validator set
+// is not changed.
+#if 0
+    if len(changes) == 0 {
+		return nil
+	}
+
+	// Check for duplicates within changes, split in 'updates' and 'deletes' lists (sorted).
+	updates, deletes, err := processChanges(changes)
+	if err != nil {
+		return err
+	}
+
+	if !allowDeletes && len(deletes) != 0 {
+		return fmt.Errorf("cannot process validators with voting power 0: %v", deletes)
+	}
+
+	// Check that the resulting set will not be empty.
+	if numNewValidators(updates, vals) == 0 && len(vals.Validators) == len(deletes) {
+		return errors.New("applying the validator changes would result in empty set")
+	}
+
+	// Verify that applying the 'deletes' against 'vals' will not result in error.
+	// Get the voting power that is going to be removed.
+	removedVotingPower, err := verifyRemovals(deletes, vals)
+	if err != nil {
+		return err
+	}
+
+	// Verify that applying the 'updates' against 'vals' will not result in error.
+	// Get the updated total voting power before removal. Note that this is < 2 * MaxTotalVotingPower
+	tvpAfterUpdatesBeforeRemovals, err := verifyUpdates(updates, vals, removedVotingPower)
+	if err != nil {
+		return err
+	}
+
+	// Compute the priorities for updates.
+	computeNewPriorities(updates, vals, tvpAfterUpdatesBeforeRemovals)
+
+	// Apply updates and removals.
+	vals.applyUpdates(updates)
+	vals.applyRemovals(deletes)
+
+	vals.updateTotalVotingPower() // will panic if total voting power > MaxTotalVotingPower
+
+	// Scale and center.
+	vals.RescalePriorities(PriorityWindowSizeFactor * vals.TotalVotingPower())
+	vals.shiftByAvgProposerPriority()
+
+	sort.Sort(ValidatorsByVotingPower(vals.Validators))
+
+	return nil
+#endif
+  }
 };
 
 }
