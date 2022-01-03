d1c9d1b5c382f38a88e2697f5738bdfe5f2bb411
nonvis@gmail.com <nonvis@gmail.com>
2022-01-03 18:56:10 +0900

    Implement update_with_change_set()

--- src/noir/net/consensus/validator.h
+++ src/noir/net/consensus/validator.h
@@ -100,103 +100,229 @@ struct validator_set {
     return val_with_most_priority;
   }
 
-  void update_with_change_set() {
-// UpdateWithChangeSet attempts to update the validator set with 'changes'.
-// It performs the following steps:
-// - validates the changes making sure there are no duplicates and splits them in updates and deletes
-// - verifies that applying the changes will not result in errors
-// - computes the total voting power BEFORE removals to ensure that in the next steps the priorities
-//   across old and newly added validators are fair
-// - computes the priorities of new validators against the final set
-// - applies the updates against the validator set
-// - applies the removals against the validator set
-// - performs scaling and centering of priority values
-// If an error is detected during verification steps, it is returned and the validator set
-// is not changed.
-#if 0
-    if len(changes) == 0 {
-        return nil
+  /** \brief Merges the vals' validator list with the updates list.
+   * When two elements with same address are seen, the one from updates is selected.
+   * Expects updates to be a list of updates sorted by address with no duplicates or errors,
+   * must have been validated with verifyUpdates() and priorities computed with computeNewPriorities().
+   */
+  void apply_updates(std::vector<validator> updates) {
+    std::vector<validator> existing(validators);
+    sort(existing.begin(), existing.end(), [](validator a, validator b) {
+      return a.address < b.address;
+    });
+
+    std::vector<validator> merged;
+    auto i = 0;
+    while (!existing.empty() && !updates.empty()) {
+      if (existing[0].address < updates[0].address) {
+        merged[i] = existing[0];
+        existing.erase(existing.begin());
+      } else {
+        // apply add or update
+        merged[i] = updates[0];
+        if (existing[0].address == updates[0].address) {
+          // validator is present in both, advance existing
+          existing.erase(existing.begin());
+        }
+        updates.erase(updates.begin());
+      }
+      i++;
     }
+
+    // add the elements which are left
+    for (auto &j: existing) {
+      merged[i] = j;
+      i++;
+    }
+    // Or, add updates which are left
+    for (auto &update: updates) {
+      merged[i] = update;
+      i++;
+    }
+
+    validators.clear();
+    for (auto j = 0; j <= i; j++)
+      validators.push_back(merged[j]);
+  }
+
+  /** \brief Removes the validators specified in 'deletes' from validator set 'vals'.
+   * Should not fail as verification has been done before.
+   * Expects vals to be sorted by address (done by applyUpdates).
+   */
+  void apply_removals(std::vector<validator> deletes) {
+    std::vector<validator> existing(validators);
+
+    std::vector<validator> merged;
+    auto i = 0;
+    // Loop over deletes until we removed all of them.
+    while (!deletes.empty()) {
+      if (existing[0].address == deletes[0].address) {
+        deletes.erase(deletes.begin());
+      } else {
+        // Leave it in the resulting slice.
+        merged[i] = existing[0];
+        i++;
+      }
+      existing.erase(existing.begin());
+    }
+
+    // add the elements which are left
+    for (auto &j: existing) {
+      merged[i] = j;
+      i++;
+    }
+
+    validators.clear();
+    for (auto j = 0; j <= i; j++)
+      validators.push_back(merged[j]);
+  }
+
+  /** \brief attempts to update the validator set with 'changes'.
+   * It performs the following steps:
+   * - validates the changes making sure there are no duplicates and splits them in updates and deletes
+   * - verifies that applying the changes will not result in errors
+   * - computes the total voting power BEFORE removals to ensure that in the next steps the priorities
+   *   across old and newly added validators are fair
+   * - computes the priorities of new validators against the final set
+   * - applies the updates against the validator set
+   * - applies the removals against the validator set
+   * - performs scaling and centering of priority values
+   * If an error is detected during verification steps, it is returned and the validator set
+   * is not changed.
+   */
+  void update_with_change_set(std::vector<validator> changes, bool allow_deletes) {
+    if (changes.empty())
+      return;
 
     // Check for duplicates within changes, split in 'updates' and 'deletes' lists (sorted).
-    updates, deletes, err := processChanges(changes)
-    if err != nil {
-        return err
+    std::vector<validator> changesCopy(changes);
+    sort(changesCopy.begin(), changesCopy.end(), [](validator a, validator b) {
+      return a.address < b.address;
+    });
+    std::vector<validator> updates, deletes;
+    bytes prevAddr;
+    for (auto val_update: changesCopy) {
+      if (val_update.address == prevAddr) {
+        elog("duplicate entry ${val_update} in changes", ("val_update", val_update.address));
+        return;
+      }
+      if (val_update.voting_power < 0) {
+        elog("voting power can't be negative: ${v_power)", ("v_power", val_update.voting_power));
+        return;
+      } else if (val_update.voting_power > max_total_voting_power) {
+        elog("to prevent clipping/overflow, voting power can't be higher than max allowed: $(v_power)",
+             ("v_power", val_update.voting_power));
+        return;
+      } else if (val_update.voting_power == 0) {
+        deletes.push_back(val_update);
+      } else {
+        updates.push_back(val_update);
+      }
+      prevAddr = val_update.address;
     }
 
-    if !allowDeletes && len(deletes) != 0 {
-        return fmt.Errorf("cannot process validators with voting power 0: %v", deletes)
+    if (!allow_deletes && !deletes.empty()) {
+      elog("cannot process validators with voting power 0");
+      return;
     }
 
     // Check that the resulting set will not be empty.
-    if numNewValidators(updates, vals) == 0 && len(vals.Validators) == len(deletes) {
-        return errors.New("applying the validator changes would result in empty set")
+    auto num_new_validators = 0;
+    for (const auto &val_update: updates) {
+      if (!has_address(val_update.address)) num_new_validators++;
     }
+    if (num_new_validators == 0 && validators.size() == deletes.size()) {
+      elog("applying the validator changes would result in empty set");
+      return;
+    }
+
 
     // Verify that applying the 'deletes' against 'vals' will not result in error.
     // Get the voting power that is going to be removed.
-    removedVotingPower, err := verifyRemovals(deletes, vals)
-    if err != nil {
-        return err
+    int64_t removed_voting_power = 0;
+    for (auto val_update: deletes) {
+      auto address = val_update.address;
+      auto val = get_by_address(address);
+      if (!val.has_value()) {
+        elog("failed to find validator ${addr} to remove", ("addr", address));
+        return;
+      }
+      removed_voting_power += val->voting_power;
+    }
+    if (deletes.size() > validators.size()) {
+      throw std::runtime_error("more deletes than validators");
     }
 
     // Verify that applying the 'updates' against 'vals' will not result in error.
     // Get the updated total voting power before removal. Note that this is < 2 * MaxTotalVotingPower
-    tvpAfterUpdatesBeforeRemovals, err := verifyUpdates(updates, vals, removedVotingPower)
-    if err != nil {
-        return err
+    auto delta = [](validator &update, validator_set &vals) {
+      auto val = vals.get_by_address(update.address);
+      if (val.has_value())
+        return update.voting_power - val->voting_power;
+      return update.voting_power;
+    };
+    std::vector<validator> updatesCopy(updates);
+    sort(updatesCopy.begin(), updatesCopy.end(), [delta, this](validator a, validator b) {
+      return delta(a, *this) < delta(b, *this);
+    });
+    auto tvp_after_removals = total_voting_power - removed_voting_power;
+    for (auto val_update: updatesCopy) {
+      tvp_after_removals += delta(val_update, *this);
+      if (tvp_after_removals > max_total_voting_power) {
+        elog("total voting power of resulting valset exceeds max");
+        return;
+      }
     }
+    auto tvp_after_updates_before_removals = tvp_after_removals + removed_voting_power;
 
     // Compute the priorities for updates.
-    compute_new_priorities(updates, vals, tvpAfterUpdatesBeforeRemovals)
+    compute_new_priorities(updates, tvp_after_updates_before_removals);
 
     // Apply updates and removals.
-    vals.applyUpdates(updates)
-    vals.applyRemovals(deletes)
+    apply_updates(updates);
+    apply_removals(deletes);
 
-    vals.updateTotalVotingPower() // will panic if total voting power > MaxTotalVotingPower
+    update_total_voting_power();
 
     // Scale and center.
-    vals.RescalePriorities(PriorityWindowSizeFactor * vals.TotalVotingPower())
-    vals.shiftByAvgProposerPriority()
+    rescale_priorities(priority_window_size_factor * get_total_voting_power());
+    shift_by_avg_proposer_priority();
 
-    sort.Sort(ValidatorsByVotingPower(vals.Validators))
-
-    return nil
-#endif
+    sort(validators.begin(), validators.end(), [](validator a, validator b) {
+      return a.voting_power < b.voting_power;
+    });
   }
 
-  void compute_new_priorities(std::vector<validator> updates, validator_set &vals, int64_t updated_total_voting_power) {
-// computeNewPriorities computes the proposer priority for the validators not present in the set based on
-// 'updatedTotalVotingPower'.
-// Leaves unchanged the priorities of validators that are changed.
-//
-// 'updates' parameter must be a list of unique validators to be added or updated.
-//
-// 'updatedTotalVotingPower' is the total voting power of a set where all updates would be applied but
-//   not the removals. It must be < 2*MaxTotalVotingPower and may be close to this limit if close to
-//   MaxTotalVotingPower will be removed. This is still safe from overflow since MaxTotalVotingPower is maxInt64/8.
-//
-// No changes are made to the validator set 'vals'.
-#if 0
-    for _, valUpdate := range updates {
-        address := valUpdate.Address
-        _, val := vals.GetByAddress(address)
-        if val == nil {
-            // add val
-            // Set ProposerPriority to -C*totalVotingPower (with C ~= 1.125) to make sure validators can't
-            // un-bond and then re-bond to reset their (potentially previously negative) ProposerPriority to zero.
-            //
-            // Contract: updatedVotingPower < 2 * MaxTotalVotingPower to ensure ProposerPriority does
-            // not exceed the bounds of int64.
-            //
-            // Compute ProposerPriority = -1.125*totalVotingPower == -(updatedVotingPower + (updatedVotingPower >> 3)).
-            valUpdate.ProposerPriority = -(updatedTotalVotingPower + (updatedTotalVotingPower >> 3))
-        } else {
-            valUpdate.ProposerPriority = val.ProposerPriority
-        }
+  /** \brief computes the proposer priority for the validators not present in the set based on
+   * 'updated_total_voting_power'.
+   * Leaves unchanged the priorities of validators that are changed.
+   *
+   * 'updates' parameter must be a list of unique validators to be added or updated.
+   *
+   * 'updated_total_voting_power' is the total voting power of a set where all updates would be applied but
+   *   not the removals. It must be < 2*MaxTotalVotingPower and may be close to this limit if close to
+   *   MaxTotalVotingPower will be removed. This is still safe from overflow since MaxTotalVotingPower is maxInt64/8.
+   *
+   * No changes are made to the validator set 'vals'.
+   */
+  void compute_new_priorities(std::vector<validator> updates, int64_t updated_total_voting_power) {
+    for (auto val_update: updates) {
+      auto address = val_update.address;
+      auto val = get_by_address(address);
+      if (!val.has_value()) {
+        // add val
+        // Set ProposerPriority to -C*totalVotingPower (with C ~= 1.125) to make sure validators can't
+        // un-bond and then re-bond to reset their (potentially previously negative) ProposerPriority to zero.
+        //
+        // Contract: updatedVotingPower < 2 * MaxTotalVotingPower to ensure ProposerPriority does
+        // not exceed the bounds of int64.
+        //
+        // Compute ProposerPriority = -1.125*totalVotingPower == -(updatedVotingPower + (updatedVotingPower >> 3)).
+        val_update.proposer_priority = -(updated_total_voting_power + (updated_total_voting_power >> 3));
+      } else {
+        val_update.proposer_priority = val->proposer_priority;
+      }
     }
-#endif
   }
 
   void increment_proposer_priority(int32_t times) {
@@ -227,8 +353,9 @@ struct validator_set {
     }
   }
 
-// rescale_priorities rescales the priorities such that the distance between the
-// maximum and minimum is smaller than `diffMax`. throws if validator set is empty.
+  /** \brief rescales the priorities such that the distance between the
+   * maximum and minimum is smaller than `diffMax`. throws if validator set is empty.
+   */
   void rescale_priorities(int64_t diff_max) {
     if (validators.empty()) throw std::runtime_error("empty validator set");
     if (diff_max <= 0) return;
