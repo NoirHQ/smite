bb928453d8c4f6be52fb01db6d48b86beb4c7afc
nonvis@gmail.com <nonvis@gmail.com>
2022-01-06 10:51:26 +0900

    docs: add license header to source files

--- src/noir/net/consensus/round_state.h
+++ src/noir/net/consensus/round_state.h
@@ -1,3 +1,8 @@
+// This file is part of NOIR.
+//
+// Copyright (c) 2022 Haderech Pte. Ltd.
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
 #pragma once
 #include <noir/net/types.h>
 #include <noir/net/protocol.h>
