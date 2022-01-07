34d5a50076426f81a8d1de1c46f22ec220c6b4b0
Sungtaek Jacob Hong <taxi2se@gmail.com>
2022-01-07 17:13:05 +0900

    feat(consensus): add simple database for state

---
+++ src/noir/consensus/db/db_test.cpp
@@ -0,0 +1,145 @@
+// This file is part of NOIR.
+//
+// Copyright (c) 2022 Haderech Pte. Ltd.
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+#include <noir/consensus/db/db.h>
+#include <catch2/catch_all.hpp>
+
+TEST_CASE("get/has/set", "[simple_db]") {
+  noir::consensus::simple_db test_db;
+  auto tests = std::to_array<std::pair<std::string, std::string>>({
+    {"hello", "world"},
+    {"key1", "val1"},
+    {"key2", "val2"},
+    {"key3", "val3"},
+    {"key4", "val4"},
+    {"key5", "val5"},
+    {"key6", "val6"},
+    {"key7", "val7"},
+    {"key8", "val8"},
+    {"key9", "val9"},
+  });
+
+  std::for_each(tests.begin(), tests.end(), [&](const auto& t) {
+    noir::p2p::bytes key(t.first.begin(), t.first.end());
+    noir::p2p::bytes val(t.second.begin(), t.second.end());
+    noir::p2p::bytes expected;
+    bool ret;
+
+    test_db.has(key, ret);
+    CHECK(ret == false);
+    ret = test_db.get(key, expected);
+    CHECK(ret == false);
+
+    ret = test_db.set(key, val);
+    CHECK(ret == true);
+    test_db.has(key, ret);
+    CHECK(ret == true);
+    ret = test_db.get(key, expected);
+    CHECK(ret == true);
+    CHECK(val == expected);
+  });
+}
+
+TEST_CASE("get/has/del", "[simple_db]") {
+  noir::consensus::simple_db test_db;
+  auto tests = std::to_array<std::pair<std::string, std::string>>({
+    {"hello", "world"},
+    {"key1", "val1"},
+    {"key2", "val2"},
+    {"key3", "val3"},
+    {"key4", "val4"},
+    {"key5", "val5"},
+    {"key6", "val6"},
+    {"key7", "val7"},
+    {"key8", "val8"},
+    {"key9", "val9"},
+  });
+
+  std::for_each(tests.begin(), tests.end(), [&](const auto& t) {
+    noir::p2p::bytes key(t.first.begin(), t.first.end());
+    noir::p2p::bytes val(t.second.begin(), t.second.end());
+    noir::p2p::bytes expected;
+    bool ret;
+
+    ret = test_db.set(key, val);
+    CHECK(ret == true);
+    test_db.has(key, ret);
+    CHECK(ret == true);
+    ret = test_db.get(key, expected);
+    CHECK(ret == true);
+    CHECK(val == expected);
+
+    ret = test_db.del(key);
+
+    test_db.has(key, ret);
+    CHECK(ret == false);
+    ret = test_db.get(key, expected);
+    CHECK(ret == false);
+  });
+}
+
+TEST_CASE("batch", "[simple_db]") {
+  noir::consensus::simple_db test_db{};
+  auto tests = std::to_array<std::pair<std::string, std::string>>({
+    {"hello", "world"},
+    {"key1", "val1"},
+    {"key2", "val2"},
+    {"key3", "val3"},
+    {"key4", "val4"},
+    {"key5", "val5"},
+    {"key6", "val6"},
+    {"key7", "val7"},
+    {"key8", "val8"},
+    {"key9", "val9"},
+  });
+
+  auto batch = test_db.new_batch();
+
+  std::for_each(tests.begin(), tests.end(), [&](const auto& t) {
+    noir::p2p::bytes key(t.first.begin(), t.first.end());
+    noir::p2p::bytes val(t.second.begin(), t.second.end());
+    noir::p2p::bytes expected;
+    bool ret;
+
+
+    ret = batch->set(key, val);
+    CHECK(ret == true);
+  });
+
+  batch->write();
+  auto batch2 = test_db.new_batch();
+
+  std::for_each(tests.begin(), tests.end(), [&](const auto& t) {
+    noir::p2p::bytes key(t.first.begin(), t.first.end());
+    noir::p2p::bytes val(t.second.begin(), t.second.end());
+    noir::p2p::bytes expected;
+    bool ret;
+
+    test_db.has(key, ret);
+    CHECK(ret == true);
+    ret = test_db.get(key, expected);
+    CHECK(ret == true);
+    CHECK(val == expected);
+
+    ret = batch2->del(key);
+    CHECK(ret == true);
+  });
+
+  batch->close();
+  batch2->write();
+
+  std::for_each(tests.begin(), tests.end(), [&](const auto& t) {
+    noir::p2p::bytes key(t.first.begin(), t.first.end());
+    noir::p2p::bytes val(t.second.begin(), t.second.end());
+    noir::p2p::bytes expected;
+    bool ret;
+
+
+    test_db.has(key, ret);
+    CHECK(ret == false);
+    ret = test_db.get(key, expected);
+    CHECK(ret == false);
+  });
+}
