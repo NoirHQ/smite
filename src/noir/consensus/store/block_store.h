64031748697d1d7d5f9ee5304b1585b7624b0ed5
Sungtaek Jacob Hong <taxi2se@gmail.com>
2022-01-10 17:42:59 +0900

    feat(consensus): add state/block store for consensus

---
+++ src/noir/consensus/store/block_store.h
@@ -0,0 +1,48 @@
+// This file is part of NOIR.
+//
+// Copyright (c) 2022 Haderech Pte. Ltd.
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+#pragma once
+
+#include <noir/consensus/block.h>
+#include <noir/consensus/types.h>
+#include <noir/consensus/db/db.h>
+
+namespace noir::consensus {
+
+class block_meta;
+
+class block_store {
+public:
+  explicit block_store(const std::string& db_type = "simple") : _db(new noir::consensus::simple_db) {}
+
+  block_store(block_store&& other) noexcept: _db(std::move(other._db)) {}
+
+  int64_t base() const { return 0; }
+
+  int64_t height() const { return 0; }
+
+  int64_t size() const { return 0; }
+
+  block_meta* load_block_meta(int64_t height) const { return nullptr; }
+
+  block* load_block(int64_t height) const { return nullptr; }
+
+  void save_block(block* bl, part_set* ps, commit* seen_commit) {}
+
+  bool prune_blocks(int64_t height) { return true; }
+
+  block* load_block_by_hash(const noir::p2p::bytes& hash) const { return nullptr; }
+
+  part* load_block_part(int64_t height, int index) const { return nullptr; }
+
+  commit* load_block_commit(int64_t height) const { return nullptr; }
+
+  commit* load_seen_commit() const { return nullptr; }
+
+private:
+  std::unique_ptr<noir::consensus::db> _db;
+};
+
+} // namespace noir::consensus
