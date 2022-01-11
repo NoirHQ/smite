6771c29a4f1ca1d02964904ef26699edf621420c
nonvis@gmail.com <nonvis@gmail.com>
2022-01-11 20:42:42 +0900

    feat: add timer that transitions various consensus states

---
+++ src/noir/consensus/consensus_state_test.cpp
@@ -0,0 +1,14 @@
+// This file is part of NOIR.
+//
+// Copyright (c) 2022 Haderech Pte. Ltd.
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+#include <catch2/catch_all.hpp>
+#include <noir/consensus/consensus_state.h>
+
+using namespace noir::consensus;
+
+TEST_CASE("Basic", "[consensus_state]") {
+  consensus_state cs;
+
+}
