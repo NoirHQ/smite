08f9aa834ece88b6e650bfd1d9c66e8e897a8088
Sungtaek Jacob Hong <taxi2se@gmail.com>
2022-01-18 14:46:25 +0900

    feat: add common scope_exit

---
+++ src/noir/common/scope_exit.h
@@ -0,0 +1,14 @@
+// This file is part of NOIR.
+//
+// Copyright (c) 2022 Haderech Pte. Ltd.
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+#pragma once
+#include <fc/scoped_exit.hpp>
+
+namespace noir {
+
+using ::fc::make_scoped_exit;
+using ::fc::scoped_exit;
+
+} // namespace noir
