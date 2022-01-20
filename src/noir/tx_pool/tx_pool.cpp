9978e09f57a15b7df534549e65ed03c748490a09
Inhyeok Lee <lazka33@gmail.com>
2022-01-20 17:54:28 +0900

    feat(tx_pool): add tx_pool class

---
+++ src/noir/tx_pool/tx_pool.cpp
@@ -0,0 +1,10 @@
+// This file is part of NOIR.
+//
+// Copyright (c) 2022 Haderech Pte. Ltd.
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+#include <noir/tx_pool/tx_pool.h>
+
+namespace noir::tx_pool {
+
+} // namespace noir::tx_pool
