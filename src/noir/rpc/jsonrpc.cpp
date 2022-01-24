db0466364783c55e2ec5abf95a4343b4b3ac4840
code0xff <ian.jungyong.um@gmail.com>
2022-01-24 18:33:17 +0900

    style(jsonrpc): reformat

--- src/noir/rpc/jsonrpc.cpp
+++ src/noir/rpc/jsonrpc.cpp
@@ -11,26 +11,24 @@ namespace noir::rpc {
 using namespace appbase;
 using namespace noir::jsonrpc;
 
-class jsonrpc_impl: std::enable_shared_from_this<jsonrpc_impl> {
+class jsonrpc_impl : std::enable_shared_from_this<jsonrpc_impl> {
 public:
   endpoint& get_or_create_endpoint(const std::string& url) {
-    if( endpoints.find(url) != endpoints.end() )
+    if (endpoints.find(url) != endpoints.end())
       return *endpoints[url];
 
     endpoints[url] = std::make_unique<endpoint>();
 
-    app().get_plugin<rpc>().add_api({{
-      url,
-      [&](std::string url, std::string body, url_response_callback cb) mutable {
-        try {
-          if( body.empty() ) body = "{}";
-          auto result = endpoints[url]->handle_request(body);
-          cb(200, result);
-        } catch( ... ) {
-          rpc::handle_exception("jsonrpc", "jsonrpc", body, cb);
-        }
-      }
-    }});
+    app().get_plugin<rpc>().add_api({{url, [&](std::string url, std::string body, url_response_callback cb) mutable {
+                                        try {
+                                          if (body.empty())
+                                            body = "{}";
+                                          auto result = endpoints[url]->handle_request(body);
+                                          cb(200, result);
+                                        } catch (...) {
+                                          rpc::handle_exception("jsonrpc", "jsonrpc", body, cb);
+                                        }
+                                      }}});
     return *endpoints[url];
   }
 
@@ -38,7 +36,7 @@ private:
   std::map<std::string, std::unique_ptr<endpoint>> endpoints;
 };
 
-jsonrpc::jsonrpc(): my(new jsonrpc_impl()) {}
+jsonrpc::jsonrpc() : my(new jsonrpc_impl()) {}
 jsonrpc::~jsonrpc() {}
 
 void jsonrpc::plugin_initialize(const CLI::App& cli, const CLI::App& config) {}
