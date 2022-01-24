38be1ddc3f9210ab11e8679265acf22f6a010365
Inhyeok Lee <lazka33@gmail.com>
2022-01-24 15:26:08 +0900

    feat(tx_pool): update tx_pool class

--- src/noir/tx_pool/tx_pool.cpp
+++ src/noir/tx_pool/tx_pool.cpp
@@ -4,8 +4,43 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 //
 #include <noir/tx_pool/tx_pool.h>
-#include <noir/tx_pool/unapplied_tx_queue.hpp>
 
 namespace noir::tx_pool {
+
+tx_pool::tx_pool()
+  : config_(config{}), tx_queue_(config_.cache_size * config_.max_tx_bytes), precheck_(nullptr), postcheck_(nullptr) {}
+
+tx_pool::tx_pool(const config& cfg)
+  : config_(cfg), tx_queue_(config_.cache_size * config_.max_tx_bytes), precheck_(nullptr), postcheck_(nullptr) {}
+
+void tx_pool::set_precheck(precheck_func* precheck) {
+  precheck_ = precheck;
+}
+
+void tx_pool::set_postcheck(postcheck_func* postcheck) {
+  postcheck_ = postcheck;
+}
+
+bool tx_pool::check_tx(const consensus::tx_ptr& tx_ptr) {
+  return true;
+}
+
+consensus::tx_ptrs tx_pool::reap_max_bytes_max_gas(uint64_t max_bytes, uint64_t max_gas) {
+  return {};
+}
+
+consensus::tx_ptrs tx_pool::reap_max_txs(uint64_t tx_count) {
+  return {};
+}
+
+bool tx_pool::update(uint64_t block_height, consensus::tx_ptrs& tx_ptrs) {
+  return true;
+}
+
+size_t tx_pool::size() const {
+}
+
+void tx_pool::flush() {
+}
 
 } // namespace noir::tx_pool
