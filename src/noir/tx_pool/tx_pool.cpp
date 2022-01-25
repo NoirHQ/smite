9bf8b823031cd3de8f1f582f3264c51a306d435a
Inhyeok Lee <lazka33@gmail.com>
2022-01-25 11:46:26 +0900

    feat(tx_pool): update check_tx

--- src/noir/tx_pool/tx_pool.cpp
+++ src/noir/tx_pool/tx_pool.cpp
@@ -8,10 +8,16 @@
 namespace noir::tx_pool {
 
 tx_pool::tx_pool()
-  : config_(config{}), tx_queue_(config_.cache_size * config_.max_tx_bytes), precheck_(nullptr), postcheck_(nullptr) {}
+  : config_(config{}), tx_queue_(config_.cache_size * config_.max_tx_bytes), precheck_(nullptr), postcheck_(nullptr) {
+  thread_ = std::make_unique<named_thread_pool>("tx_pool", 1);
+}
 
 tx_pool::tx_pool(const config& cfg)
   : config_(cfg), tx_queue_(config_.cache_size * config_.max_tx_bytes), precheck_(nullptr), postcheck_(nullptr) {}
+
+tx_pool::~tx_pool() {
+  thread_.reset();
+}
 
 void tx_pool::set_precheck(precheck_func* precheck) {
   precheck_ = precheck;
@@ -21,8 +27,39 @@ void tx_pool::set_postcheck(postcheck_func* postcheck) {
   postcheck_ = postcheck;
 }
 
-bool tx_pool::check_tx(const consensus::tx_ptr& tx_ptr) {
-  return true;
+std::optional<consensus::abci::response_check_tx> tx_pool::check_tx(const consensus::tx_ptr& tx_ptr, bool sync) {
+  if (tx_ptr->size() > config_.max_tx_bytes) {
+    return std::nullopt;
+  }
+
+  if (precheck_ && !precheck_(*tx_ptr)) {
+    return std::nullopt;
+  }
+
+  consensus::abci::response_check_tx res;
+  res.result = async_thread_pool(thread_->get_executor(), [&res, tx_ptr, this](){
+    if (postcheck_ && !postcheck_(*tx_ptr, res)) {
+      if (res.code != consensus::abci::code_type_ok) {
+        return false;
+      }
+
+      // add tx to failed tx_pool
+    }
+
+    if (!res.sender.empty()) {
+      // check tx_pool has sender
+    }
+
+    std::lock_guard<std::mutex> lock(mutex_);
+    return tx_queue_.add_tx(tx_ptr);
+  });
+
+  if (sync) {
+    if (!res.result.get()) {
+      return std::nullopt;
+    }
+  }
+  return res;
 }
 
 consensus::tx_ptrs tx_pool::reap_max_bytes_max_gas(uint64_t max_bytes, uint64_t max_gas) {
