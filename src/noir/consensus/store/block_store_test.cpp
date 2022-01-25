0d9fea39fa79ae5391dbfbbd45d190d5baae0bf0
Sungtaek Jacob Hong <taxi2se@gmail.com>
2022-01-25 14:31:35 +0900

    block store interface

--- src/noir/consensus/store/block_store_test.cpp
+++ src/noir/consensus/store/block_store_test.cpp
@@ -8,21 +8,37 @@
 #include <noir/consensus/store/store_test.h>
 
 namespace {
-TEST_CASE("block_store", "[store]") {
-  noir::consensus::block_store bls("simple");
+
+TEST_CASE("block_store", "[block_store]") {
+  noir::consensus::block_store bls(
+    std::make_shared<noir::db::session::session<noir::db::session::rocksdb_t>>(make_session()));
 
   CHECK_NOTHROW(bls.base());
   CHECK_NOTHROW(bls.height());
   CHECK_NOTHROW(bls.size());
-  CHECK_NOTHROW(bls.load_block_meta(0));
-  CHECK_NOTHROW(bls.load_block(0));
-  CHECK_NOTHROW(bls.save_block(nullptr, nullptr, nullptr));
-  CHECK_NOTHROW(bls.prune_blocks(0));
+  {
+    noir::consensus::block_meta block_meta_{};
+    CHECK_NOTHROW(bls.load_base_meta(block_meta_));
+    CHECK_NOTHROW(bls.load_block_meta(0, block_meta_));
+  }
+  {
+    noir::consensus::block bl_{};
+    noir::consensus::part_set p_set_{};
+    noir::consensus::commit commit_{};
+    CHECK_NOTHROW(bls.save_block(bl_, p_set_, commit_));
+    CHECK_NOTHROW(bls.load_block(0, bl_));
 
-  CHECK_NOTHROW(bls.load_block_by_hash(noir::p2p::bytes{}));
-  CHECK_NOTHROW(bls.load_block_part(0, 0));
-  CHECK_NOTHROW(bls.load_block_commit(0));
-  CHECK_NOTHROW(bls.load_seen_commit());
+    CHECK_NOTHROW(bls.load_block_by_hash(noir::p2p::bytes{}, bl_));
+
+    noir::consensus::part part_{};
+    CHECK_NOTHROW(bls.load_block_part(0, 0, part_));
+    CHECK_NOTHROW(bls.load_block_commit(0, commit_));
+    CHECK_NOTHROW(bls.load_seen_commit(commit_));
+  }
+  {
+    uint64_t pruned{0};
+    CHECK_NOTHROW(bls.prune_blocks(0, pruned));
+  }
 }
 
 } // namespace
