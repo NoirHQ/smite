99d0375e90c124f5979c7c631e18bffe5ba3b146
Sungtaek Jacob Hong <taxi2se@gmail.com>
2022-01-26 19:28:09 +0900

    divide test

--- src/noir/consensus/store/state_store_test.cpp
+++ src/noir/consensus/store/state_store_test.cpp
@@ -4,33 +4,10 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 //
 #include <catch2/catch_all.hpp>
-#include <noir/consensus/store/block_store.h>
 #include <noir/consensus/store/state_store.h>
+#include <noir/consensus/store/store_test.h>
 
 namespace {
-
-inline std::shared_ptr<rocksdb::DB> make_rocks_db(const std::string& name = "/tmp/testdb") {
-  rocksdb::DestroyDB(name.c_str(), rocksdb::Options{});
-
-  rocksdb::DB* cache_ptr{nullptr};
-  auto cache = std::shared_ptr<rocksdb::DB>{};
-
-  auto options = rocksdb::Options{};
-  options.create_if_missing = true;
-  options.level_compaction_dynamic_level_bytes = true;
-  options.bytes_per_sync = 1048576;
-  options.OptimizeLevelStyleCompaction(256ull << 20);
-
-  auto status = rocksdb::DB::Open(options, name.c_str(), &cache_ptr);
-  cache.reset(cache_ptr);
-
-  return cache;
-}
-
-inline noir::db::session::session<noir::db::session::rocksdb_t> make_session(const std::string& name = "/tmp/testdb") {
-  auto rocksdb = make_rocks_db(name);
-  return noir::db::session::make_session(std::move(rocksdb), 16);
-}
 
 inline void check_state_equal(noir::consensus::state& lhs, noir::consensus::state& rhs) {
   CHECK(lhs.version == rhs.version);
@@ -234,23 +211,6 @@ TEST_CASE("prune_state", "[db_store]") {
       }
     }
   });
-}
-
-TEST_CASE("block_store", "[store]") {
-  noir::consensus::block_store bls("simple");
-
-  CHECK_NOTHROW(bls.base());
-  CHECK_NOTHROW(bls.height());
-  CHECK_NOTHROW(bls.size());
-  CHECK_NOTHROW(bls.load_block_meta(0));
-  CHECK_NOTHROW(bls.load_block(0));
-  CHECK_NOTHROW(bls.save_block(nullptr, nullptr, nullptr));
-  CHECK_NOTHROW(bls.prune_blocks(0));
-
-  CHECK_NOTHROW(bls.load_block_by_hash(noir::p2p::bytes{}));
-  CHECK_NOTHROW(bls.load_block_part(0, 0));
-  CHECK_NOTHROW(bls.load_block_commit(0));
-  CHECK_NOTHROW(bls.load_seen_commit());
 }
 
 } // namespace
