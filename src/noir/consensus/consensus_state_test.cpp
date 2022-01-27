364cc431b03373990c20c02b3749584efd50c20e
nonvis@gmail.com <nonvis@gmail.com>
2022-01-27 12:24:30 +0800

    feat: add a test case for no priv_validator

--- src/noir/consensus/consensus_state_test.cpp
+++ src/noir/consensus/consensus_state_test.cpp
@@ -38,3 +38,18 @@ TEST_CASE("Proposer Selection 0", "[consensus_state]") {
   //  cs1->schedule_timeout(std::chrono::milliseconds{3000}, 1, 0, NewHeight);
   //  cs1->schedule_timeout(std::chrono::milliseconds{4000}, 1, 1, Propose);
 }
+
+TEST_CASE("No Priv Validator", "[consensus_state]") {
+  auto local_config = config_setup();
+  auto [cs1, vss] = rand_cs(local_config, 1);
+  cs1->local_priv_validator = {};
+
+  auto height = cs1->rs.height;
+  auto round = cs1->rs.round;
+
+  start_test_round(cs1, height, round);
+
+  force_tick(cs1); // ensureNewRound
+
+  CHECK(!cs1->get_round_state()->proposal.has_value());
+}
