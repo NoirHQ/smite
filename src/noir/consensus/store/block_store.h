3534e2db396922e3ce2955c01ea26d2b71279cd2
Sungtaek Jacob Hong <taxi2se@gmail.com>
2022-01-28 14:58:06 +0900

    block store

--- src/noir/consensus/store/block_store.h
+++ src/noir/consensus/store/block_store.h
@@ -5,7 +5,12 @@
 //
 #pragma once
 
+#include <noir/common/for_each.h>
 #include <noir/consensus/block.h>
+
+#include <noir/codec/scale.h>
+
+#include <noir/common/hex.h>
 #include <noir/consensus/types.h>
 #include <noir/db/rocks_session.hpp>
 #include <noir/db/session.hpp>
@@ -19,7 +24,14 @@ struct block_meta {
   noir::consensus::block_header header;
   int32_t num_txs;
   static block_meta new_block_meta(const block& bl_, const part_set& bl_parts) {
-    return {};
+    auto hash_ = const_cast<block&>(bl_).get_hash();
+    auto parts_ = const_cast<part_set&>(bl_parts);
+    return {
+      .bl_id = noir::p2p::block_id{.hash{hash_}, .parts{parts_.header()}},
+      .bl_size = static_cast<int32_t>(noir::codec::scale::encode_size(bl_)),
+      .header = bl_.header,
+      .num_txs = 0, // TODO: bl_.data.size()
+    };
   }
 };
 
@@ -55,25 +67,53 @@ public:
   /// \brief gets the first known contiguous block height, or 0 for empty block stores.
   /// \return base height
   int64_t base() const {
-    return 0;
+    auto begin_it = db_session_->lower_bound_from_bytes(encode_key<prefix::block_meta>(1));
+    auto end_it = db_session_->lower_bound_from_bytes(encode_key<prefix::block_meta>(0x1ll << 63));
+    if (begin_it == end_it) {
+      return 0;
+    }
+    auto tmp = begin_it.key();
+    noir::p2p::bytes key_{tmp.begin(), tmp.end()};
+    int64_t height_;
+    assert(decode_block_meta_key(key_, height_)); // TODO: handle panic in consensus
+    return height_;
   }
 
   /// \brief gets the last known contiguous block height, or 0 for empty block stores.
   /// \return height
   int64_t height() const {
-    return 0;
+    auto begin_it = db_session_->lower_bound_from_bytes(encode_key<prefix::block_meta>(0));
+    auto end_it = db_session_->lower_bound_from_bytes(encode_key<prefix::block_meta>(0x1ll << 63));
+    if (begin_it == end_it) { // no iterator found
+      return 0;
+    }
+    auto tmp = (--end_it).key();
+    noir::p2p::bytes key_{tmp.begin(), tmp.end()};
+    int64_t height_;
+    assert(decode_block_meta_key(key_, height_)); // TODO: handle panic in consensus
+    return height_;
   }
 
   /// \brief gets the number of blocks in the block store.
   /// \return size
   int64_t size() const {
-    return 0;
+    int64_t height_ = height();
+    return (height_ == 0) ? 0 : height_ + 1 - base();
   }
 
   /// \brief loads the base block meta
   /// \param[out] block_meta loaded block_meta object
   /// \return true on success, false otherwise
   bool load_base_meta(block_meta& bl_meta) const {
+    auto tmp_it = db_session_->lower_bound_from_bytes(encode_key<prefix::block_meta>(1));
+    auto tmp_bytes = tmp_it.key();
+    noir::p2p::bytes key_{tmp_bytes.begin(), tmp_bytes.end()};
+    int64_t height_;
+    if (!decode_block_meta_key(key_, height_) || ((*tmp_it).second == std::nullopt)) {
+      return false;
+    }
+    tmp_bytes = (*tmp_it).second.value();
+    bl_meta = noir::codec::scale::decode<block_meta>(noir::p2p::bytes{tmp_bytes.begin(), tmp_bytes.end()});
     return true;
   }
 
@@ -82,6 +122,22 @@ public:
   /// \param[out] bl loaded block object
   /// \return true on success, false otherwise
   bool load_block(int64_t height_, block& bl) const {
+    block_meta bl_meta{};
+    if (!load_block_meta(height_, bl_meta)) {
+      return false;
+    }
+    auto parts_total = bl_meta.bl_id.parts.total;
+    for (auto i = 0; i < parts_total; ++i) {
+      part part_{};
+      // If the part is missing (e.g. since it has been deleted after we
+      // loaded the block meta) we consider the whole block to be missing.
+      if (!load_block_part(height_, i, part_)) {
+        return false;
+      }
+      if (!parse_part_to_block(part_, bl)) {
+        return false;
+      }
+    }
     return true;
   }
 
@@ -90,7 +146,12 @@ public:
   /// \param[out] bl loaded block object
   /// \return true on success, false otherwise
   bool load_block_by_hash(const noir::p2p::bytes& hash, block& bl) const {
-    return true;
+    auto tmp = db_session_->read_from_bytes(encode_key<prefix::block_hash>(hash));
+    if (!tmp.has_value()) {
+      return false;
+    }
+    auto height_ = noir::codec::scale::decode<int64_t>(tmp.value());
+    return load_block(height_, bl);
   }
 
   /// \brief loads the Part at the given index from the block at the given height.
@@ -99,6 +160,12 @@ public:
   /// \param[out] part_ loaded part object
   /// \return true on success, false otherwise
   bool load_block_part(int64_t height_, int index, part& part_) const {
+    auto tmp = db_session_->read_from_bytes(encode_key<prefix::block_part>(height_, index));
+    if ((!tmp.has_value()) || tmp.value().empty()) {
+      return false;
+    }
+    part_ = noir::codec::scale::decode<part>(tmp.value());
+    part_.proof = noir::p2p::bytes{}; // TODO: codec merkle proof
     return true;
   }
 
@@ -107,6 +174,11 @@ public:
   /// \param[out] block_meta_ loaded block_meta object
   /// \return true on success, false otherwise
   bool load_block_meta(int64_t height_, block_meta& block_meta_) const {
+    auto tmp = db_session_->read_from_bytes(encode_key<prefix::block_meta>(height_));
+    if ((!tmp.has_value()) || tmp.value().empty()) {
+      return false;
+    }
+    block_meta_ = noir::codec::scale::decode<block_meta>(tmp.value());
     return true;
   }
 
@@ -115,6 +187,11 @@ public:
   /// \param[out] commit_ loaded commit object
   /// \return true on success, false otherwise
   bool load_block_commit(int64_t height_, commit& commit_) const {
+    auto tmp = db_session_->read_from_bytes(encode_key<prefix::block_commit>(height_));
+    if ((!tmp.has_value()) || tmp.value().empty()) {
+      return false;
+    }
+    commit_ = noir::codec::scale::decode<commit>(tmp.value());
     return true;
   }
 
@@ -124,6 +201,11 @@ public:
   /// \param[out] commit_ loaded commit object
   /// \return true on success, false otherwise
   bool load_seen_commit(commit& commit_) const {
+    auto tmp = db_session_->read_from_bytes(encode_key<prefix::seen_commit>());
+    if ((!tmp.has_value()) || tmp.value().empty()) {
+      return false;
+    }
+    commit_ = noir::codec::scale::decode<commit>(tmp.value());
     return true;
   }
 
@@ -138,6 +220,38 @@ public:
   /// \param[in] seen_commit commit object
   /// \return true on success, false otherwise
   bool save_block(const block& bl, const part_set& bl_parts, const commit& seen_commit) {
+    auto height_ = bl.header.height;
+    auto hash_ = const_cast<block&>(bl).get_hash();
+    auto parts_ = const_cast<part_set&>(bl_parts);
+    assert((base() == 0) || (height_ == height() + 1)); // TODO: might need reserve() to optimize
+    assert(parts_.is_complete()); // TODO: might need reserve() to optimize
+
+    // Save block parts. This must be done before the block meta, since callers
+    // typically load the block meta first as an indication that the block exists
+    // and then go on to load block parts - we must make sure the block is
+    // complete as soon as the block meta is written.
+    for (auto i = 0; i < parts_.total; i++) {
+      const auto part = parts_.get_part(i);
+      save_block_part(height_, i, part);
+    }
+
+    {
+      block_meta bl_meta = block_meta::new_block_meta(bl, bl_parts);
+      auto buf = noir::codec::scale::encode(bl_meta);
+      db_session_->write_from_bytes(encode_key<prefix::block_meta>(height_), buf);
+      db_session_->write_from_bytes(encode_key<prefix::block_hash>(hash_), encode_val(height_));
+    }
+    {
+      commit commit_{}; // TODO: bl.last_commit
+      auto buf = noir::codec::scale::encode(commit_);
+      db_session_->write_from_bytes(encode_key<prefix::block_commit>(height_ - 1), buf);
+    }
+    // Save seen commit (seen +2/3 precommits for block)
+    {
+      auto buf = noir::codec::scale::encode(seen_commit);
+      db_session_->write_from_bytes(encode_key<prefix::seen_commit>(), buf);
+    }
+    db_session_->commit();
     return true;
   }
 
@@ -146,6 +260,9 @@ public:
   /// \param[in] seen_commit commit object to save
   /// \return true on success, false otherwise
   bool save_seen_commit(int64_t height_, const commit& seen_commit) {
+    auto buf = noir::codec::scale::encode(seen_commit);
+    db_session_->write_from_bytes(encode_key<prefix::seen_commit>(), buf);
+    db_session_->commit();
     return true;
   }
 
@@ -154,6 +271,29 @@ public:
   /// \param[in] block_id_ block_id object to save
   /// \return true on success, false otherwise
   bool save_signed_header(const signed_header& header, const noir::p2p::block_id& block_id_) {
+    auto height_ = header.header.height;
+    auto tmp = db_session_->find_from_bytes(encode_key<prefix::block_meta>(height_));
+    if (tmp == db_session_->end()) {
+      return false;
+    }
+
+    // FIXME: saving signed headers although necessary for proving evidence,
+    // doesn't have complete parity with block meta's thus block size and num
+    // txs are filled with negative numbers. We should aim to find a solution to
+    // this.
+    block_meta bm{
+      .bl_id = block_id_,
+      .bl_size = -1,
+      .header = header.header,
+      .num_txs = -1,
+    };
+
+    auto buf = noir::codec::scale::encode(bm);
+    db_session_->write_from_bytes(encode_key<prefix::block_meta>(height_), buf);
+
+    buf = noir::codec::scale::encode(header.commit);
+    db_session_->write_from_bytes(encode_key<prefix::block_commit>(height_), buf);
+    db_session_->commit();
     return true;
   }
 
@@ -162,11 +302,200 @@ public:
   /// \param[out] pruned the number of blocks pruned.
   /// \return true on success, false otherwise
   bool prune_blocks(int64_t height_, uint64_t& pruned) {
+    uint64_t tmp = 0;
+    pruned = 0;
+    if (height_ <= 0) {
+      // "height must be greater than 0"
+      return false;
+    }
+    if (height_ > height()) {
+      // "height must be equal to or less than the latest height %d", bs.Height()
+      return false;
+    }
+
+    auto remove_block_hash = [&](const auto& k, const auto& v) {
+      block_meta bm{};
+      bm = noir::codec::scale::decode<block_meta>(v);
+      db_session_->erase_from_bytes(encode_key<prefix::block_meta>(bm.bl_id.hash));
+      return true;
+    };
+    if (!prune_range(encode_key<prefix::block_meta>(0), encode_key<prefix::block_meta>(height_),
+          std::optional<pre_deletion_hook_type>(remove_block_hash), pruned)) {
+      return false;
+    }
+    if (!prune_range(
+          encode_key<prefix::block_part>(0, 0), encode_key<prefix::block_part>(height_, 0), std::nullopt, tmp)) {
+      return false;
+    }
+    // assert(pruned == tmp);
+    tmp = 0;
+    if (!prune_range(
+          encode_key<prefix::block_commit>(0), encode_key<prefix::block_commit>(height_), std::nullopt, tmp)) {
+      return false;
+    }
+    // assert(pruned == tmp);
+
     return true;
   }
 
 private:
+  enum class prefix : char {
+    block_meta = 0,
+    block_part = 1,
+    block_commit = 2,
+    seen_commit = 3,
+    block_hash = 4,
+  };
+
   std::shared_ptr<db_session_type> db_session_;
+
+  static noir::p2p::bytes encode_val(int64_t val) {
+    auto hex_ = from_hex(fmt::format("{:016x}", static_cast<uint64_t>(val)));
+    return {hex_.begin(), hex_.end()};
+  }
+
+  template<typename... int64s>
+  static noir::p2p::bytes encode_val(int64_t val, int64s... args) {
+    auto hex_ = from_hex(fmt::format("{:016x}", static_cast<uint64_t>(val)));
+    noir::p2p::bytes lhs{hex_.begin(), hex_.end()};
+    auto rhs = append_key(args...);
+    lhs.insert(lhs.end(), rhs.begin(), rhs.end());
+    return lhs;
+  }
+
+  template<prefix key_prefix>
+  static noir::p2p::bytes encode_key() {
+    noir::p2p::bytes key_{};
+    key_.push_back(static_cast<char>(key_prefix));
+    return key_;
+  }
+
+  template<prefix key_prefix>
+  static noir::p2p::bytes encode_key(int64_t val) {
+    auto lhs = encode_key<key_prefix>();
+    auto rhs = encode_val(val); // TODO: might need reserve() to optimize
+    lhs.insert(lhs.end(), rhs.begin(), rhs.end());
+    return lhs;
+  }
+
+  template<prefix key_prefix, typename... int64s>
+  static noir::p2p::bytes encode_key(int64_t val, int64s... args) {
+    auto lhs = encode_key<key_prefix>(val);
+    auto rhs = encode_val(args...); // TODO: might need reserve() to optimize
+    lhs.insert(lhs.end(), rhs.begin(), rhs.end());
+    return lhs;
+  }
+
+  template<prefix key_prefix>
+  static noir::p2p::bytes encode_key(noir::p2p::bytes bytes_) {
+    auto lhs = encode_key<key_prefix>();
+    // std::string(bytes_.begin(), bytes_.end());
+    lhs.insert(lhs.end(), bytes_.begin(), bytes_.end());
+    return lhs;
+  }
+
+  static bool decode_block_meta_key(const noir::p2p::bytes& key, int64_t& height_) {
+    static constexpr size_t size_ = sizeof(char) + sizeof(int64_t);
+    if ((key.size() != size_) || (key[0] != static_cast<char>(prefix::block_meta))) {
+      return false;
+    }
+
+    auto height_hex = to_hex(noir::p2p::bytes{key.begin() + 1, key.end()});
+    height_ = stoull(height_hex, nullptr, 16);
+    return true;
+  }
+
+  // TODO: need to decide encoding of block
+  // currently using protobuf style encoding/decoding
+  static bool parse_part_to_block(const part& part_, block& bl) {
+    auto bytes_ = part_.bytes_;
+    switch (part_.index) {
+    case 0: {
+      // Header
+      bl.header = noir::codec::scale::decode<block_header>(bytes_);
+      return true;
+      break;
+    }
+    case 1: {
+      // Data
+      // bl.data = noir::codec::scale::decode<data>(bytes_);
+      return true;
+      break;
+    }
+    case 2: {
+      // Evidence
+      // bl.evidence = noir::codec::scale::decode<evidence>(bytes_);
+      return true;
+      break;
+    }
+    case 3: {
+      // Last Commit
+      // bl.last_commit = noir::codec::scale::decode<commit>(bytes_);
+      return true;
+    }
+    default: {
+      // Cannot reach
+      return false;
+    }
+    }
+  }
+
+  bool save_block_part(int64_t height_, int index_, const part& part_) {
+    auto buf = noir::codec::scale::encode(part_);
+    db_session_->write_from_bytes(encode_key<prefix::block_part>(height_, index_), buf);
+    return true;
+  }
+
+  using pre_deletion_hook_type = std::function<bool(const noir::p2p::bytes& key, const noir::p2p::bytes& val)>;
+  bool prune_range(const noir::p2p::bytes& start, const noir::p2p::bytes& end,
+    std::optional<pre_deletion_hook_type> pre_deletion_hook, uint64_t& total_pruned) {
+    noir::p2p::bytes start_ = start;
+
+    // loop until we have finished iterating over all the keys by writing, opening a new batch
+    // and incrementing through the next range of keys.
+    while (start_ != end) {
+      uint64_t pruned{0};
+      if (!batch_delete(start_, end, pre_deletion_hook, pruned, start_)) {
+        return false;
+      }
+      total_pruned += pruned;
+      db_session_->commit();
+    }
+
+    return true;
+  }
+
+  bool batch_delete(const noir::p2p::bytes& start, const noir::p2p::bytes& end,
+    std::optional<pre_deletion_hook_type> pre_deletion_hook, uint64_t& total_pruned, noir::p2p::bytes& new_end) {
+    uint64_t pruned = 0;
+
+    auto begin_ = db_session_->lower_bound_from_bytes(start);
+    auto end_ = db_session_->lower_bound_from_bytes(end);
+
+    for (auto it = begin_; it != end_; ++it) {
+      auto key_ = it.key_from_bytes();
+      if (pre_deletion_hook.has_value()) {
+        auto val_ = it.value_from_bytes();
+        if (val_->empty()) {
+          return false;
+        }
+        if (!pre_deletion_hook.value()(key_, val_.value())) {
+          return false;
+        }
+      }
+      db_session_->erase_from_bytes(key_);
+
+      if (++pruned >= 1000) {
+        total_pruned = pruned;
+        new_end = key_;
+        return true;
+      }
+    }
+
+    total_pruned = pruned;
+    new_end = end;
+    return true;
+  }
 };
 
 } // namespace noir::consensus
