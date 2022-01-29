a4b19e0e89cbfe168bb94a66c5e282ac30036c4b
Jeeyong Um <conr2d@gmail.com>
2022-01-30 00:02:45 +0800

    fix(codec): compile error with gcc

--- src/noir/common/types/inttypes.h
+++ src/noir/common/types/inttypes.h
@@ -15,41 +15,45 @@ using int256_t = boost::multiprecision::int256_t;
 template<>
 struct is_foreachable<int256_t> : std::false_type {};
 
-template<typename DataStream>
-DataStream& operator<<(DataStream& ds, const int256_t& v) {
-  uint64_t data[4] = {0};
-  boost::multiprecision::export_bits(v, std::begin(data), 64, false);
-  ds << std::span((const char*)data, 32);
-  return ds;
-}
-
-template<typename DataStream>
-DataStream& operator>>(DataStream& ds, int256_t& v) {
-  uint64_t data[4] = {0};
-  ds >> std::span((char*)data, 32);
-  boost::multiprecision::import_bits(v, std::begin(data), std::end(data), 64, false);
-  return ds;
-}
+// XXX: disabled due to compile error with gcc
+//
+// template<typename DataStream>
+// DataStream& operator<<(DataStream& ds, const int256_t& v) {
+//   uint64_t data[4] = {0};
+//   boost::multiprecision::export_bits(v, std::begin(data), 64, false);
+//   ds << std::span((const char*)data, 32);
+//   return ds;
+// }
+// 
+// template<typename DataStream>
+// DataStream& operator>>(DataStream& ds, int256_t& v) {
+//   uint64_t data[4] = {0};
+//   ds >> std::span((char*)data, 32);
+//   boost::multiprecision::import_bits(v, std::begin(data), std::end(data), 64, false);
+//   return ds;
+// }
 
 using uint256_t = boost::multiprecision::uint256_t;
 
 template<>
 struct is_foreachable<uint256_t> : std::false_type {};
 
-template<typename DataStream>
-DataStream& operator<<(DataStream& ds, const uint256_t& v) {
-  uint64_t data[4] = {0};
-  boost::multiprecision::export_bits(v, std::begin(data), 64, false);
-  ds << std::span((const char*)data, 32);
-  return ds;
-}
-
-template<typename DataStream>
-DataStream& operator>>(DataStream& ds, uint256_t& v) {
-  uint64_t data[4] = {0};
-  ds >> std::span((char*)data, 32);
-  boost::multiprecision::import_bits(v, std::begin(data), std::end(data), 64, false);
-  return ds;
-}
+// XXX: disabled due to compile error with gcc
+//
+// template<typename DataStream>
+// DataStream& operator<<(DataStream& ds, const uint256_t& v) {
+//   uint64_t data[4] = {0};
+//   boost::multiprecision::export_bits(v, std::begin(data), 64, false);
+//   ds << std::span((const char*)data, 32);
+//   return ds;
+// }
+// 
+// template<typename DataStream>
+// DataStream& operator>>(DataStream& ds, uint256_t& v) {
+//   uint64_t data[4] = {0};
+//   ds >> std::span((char*)data, 32);
+//   boost::multiprecision::import_bits(v, std::begin(data), std::end(data), 64, false);
+//   return ds;
+// }
 
 } // namespace noir
