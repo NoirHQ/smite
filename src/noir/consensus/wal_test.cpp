d1025f7f711c962ad117ecf44da91a4883ff1450
xedonman <taxi2se@gmail.com>
2022-02-03 16:09:05 +0900

    feat(consensus): prototype wal

---
+++ src/noir/consensus/wal_test.cpp
@@ -0,0 +1,11 @@
+// This file is part of NOIR.
+//
+// Copyright (c) 2022 Haderech Pte. Ltd.
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+#include <catch2/catch_all.hpp>
+#include <noir/consensus/wal.h>
+
+using namespace noir::consensus;
+
+TEST_CASE("encode/decode", "[wal_codec]") {}
