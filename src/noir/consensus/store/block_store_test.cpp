5bf506117108256a29e5e5dcaa27c462e19e07ce
xedonman <taxi2se@gmail.com>
2022-02-03 14:11:39 +0900

    fix(consensus/store): fix block hash is not loaded

--- src/noir/consensus/store/block_store_test.cpp
+++ src/noir/consensus/store/block_store_test.cpp
@@ -94,6 +94,7 @@ TEST_CASE("save/load_block", "[block_store]") {
   noir::consensus::commit commit_{};
   for (auto height = 1; height < 100; ++height) {
     auto bl_ = make_block(height, genesis_state, commit_);
+    auto hash_ = bl_.get_hash(); // TODO: temporary workaround (block get_hash() not implemented)
     auto p_set_ = make_part_set(bl_, 2);
     auto seen_commit_ = make_commit(10, noir::p2p::tstamp{});
 
@@ -132,9 +133,9 @@ TEST_CASE("save/load_block", "[block_store]") {
 
     {
       // TODO: bl.get_hash()
-      // noir::consensus::block ret{};
-      // noir::consensus::block exp{};
-      // CHECK(bls.load_block_by_hash(exp.get_hash(), ret) == true);
+      noir::consensus::block ret{};
+      CHECK(bls.load_block_by_hash(hash_, ret) == true);
+      CHECK(ret.get_hash() == bl_.get_hash());
     }
 
     {
