c6bc4d277ce856e79aee76ce078dfadc31278cfc
Jeeyong Um <conr2d@gmail.com>
2022-02-08 19:24:36 +0800

    feat(crypto): add keccak256 hash function

---
+++ src/noir/crypto/keccak.cpp
@@ -0,0 +1,30 @@
+// This file is part of NOIR.
+//
+// Copyright (c) 2022 Haderech Pte. Ltd.
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+#include <span>
+#include <vector>
+
+extern "C" {
+#include <KeccakHash.h>
+}
+
+#define Keccak_HashInitialize_Keccak256(hashInstance) Keccak_HashInitialize(hashInstance, 1088, 512, 256, 0x01)
+
+namespace noir::crypto {
+
+void keccak256(std::span<const char> input, std::span<char> output) {
+  Keccak_HashInstance ctx;
+  Keccak_HashInitialize_Keccak256(&ctx);
+  Keccak_HashUpdate(&ctx, (BitSequence*)input.data(), input.size() * 8);
+  Keccak_HashFinal(&ctx, (BitSequence*)output.data());
+}
+
+std::vector<char> keccak256(std::span<const char> input) {
+  std::vector<char> output(32);
+  keccak256(input, output);
+  return output;
+}
+
+} // namespace noir::crypto
