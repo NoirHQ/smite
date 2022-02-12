3a3c2cf3caa9082d187e5bd598241e63e029df77
Jeeyong Um <conr2d@gmail.com>
2022-02-12 18:31:07 +0800

    feat: configure exception type when calling check

--- src/noir/common/check.h
+++ src/noir/common/check.h
@@ -10,28 +10,16 @@
 
 namespace noir {
 
+template<typename Error = std::runtime_error>
 inline void check(bool pred, std::string_view msg) {
-  if (!pred) {
-    throw std::runtime_error(msg.data());
-  }
+  if (!pred)
+    throw Error(msg.data());
 }
 
+template<typename Error = std::runtime_error>
 inline void check(bool pred, const char* msg = "") {
-  if (!pred) {
-    throw std::runtime_error(msg);
-  }
-}
-
-inline void check(bool pred, const std::string& msg) {
-  if (!pred) {
-    throw std::runtime_error(msg);
-  }
-}
-
-inline void check(bool pred, std::string&& msg) {
-  if (!pred) {
-    throw std::runtime_error(msg);
-  }
+  if (!pred)
+    throw Error(msg);
 }
 
 } // namespace noir
