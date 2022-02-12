aed39295ef9a544e3ef16cee8c0a250b0a9ff918
Jeeyong Um <conr2d@gmail.com>
2022-02-12 15:44:02 +0800

    refactor(crypto): add output buffer size check

--- src/noir/crypto/blake2.cpp
+++ src/noir/crypto/blake2.cpp
@@ -3,19 +3,27 @@
 // Copyright (c) 2022 Haderech Pte. Ltd.
 // SPDX-License-Identifier: AGPL-3.0-or-later
 //
+#include <noir/common/check.h>
 #include <noir/crypto/hash.h>
-#include <blake2.h>
 #include <openssl/evp.h>
+#include <blake2.h>
 
 namespace noir::crypto {
 
+namespace unsafe {
+  void blake2b_256(std::span<const char> in, std::span<char> out) {
+    blake2b((unsigned char*)out.data(), (unsigned char*)in.data(), nullptr, 32, in.size(), 0);
+  }
+} // namespace unsafe
+
 void blake2b_256(std::span<const char> in, std::span<char> out) {
-  blake2b((unsigned char*)out.data(), (unsigned char*)in.data(), nullptr, 32, in.size(), 0);
+  check(out.size() >= 32, "insufficient output buffer");
+  unsafe::blake2b_256(in, out);
 }
 
 std::vector<char> blake2b_256(std::span<const char> in) {
   std::vector<char> out(32);
-  blake2b_256(in, out);
+  unsafe::blake2b_256(in, out);
   return out;
 }
 
