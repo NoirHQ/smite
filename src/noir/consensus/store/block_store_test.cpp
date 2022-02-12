90f10a64c7deb8ebdd9870ad785272416973578c
nonvis@gmail.com <nonvis@gmail.com>
2022-02-12 14:10:34 +0800

    test: add a case for apply_block

--- src/noir/consensus/store/block_store_test.cpp
+++ src/noir/consensus/store/block_store_test.cpp
@@ -11,59 +11,6 @@
 
 namespace {
 
-inline std::vector<noir::consensus::tx> make_txs(int64_t height, int num) {
-  std::vector<noir::consensus::tx> txs{};
-  for (auto i = 0; i < num; ++i) {
-    txs.push_back(noir::consensus::tx{});
-  }
-  return txs;
-}
-
-inline noir::consensus::block make_block(
-  int64_t height, const noir::consensus::state& st, const noir::consensus::commit& commit_) {
-  auto txs = make_txs(st.last_block_height, 10);
-  auto [block_, part_set_] = const_cast<noir::consensus::state&>(st).make_block(height, txs, commit_, /* {}, */ {});
-  // TODO: temparary workaround to set block
-  block_.header.height = height;
-  return block_;
-}
-
-inline noir::consensus::part_set make_part_set(const noir::consensus::block& bl, uint32_t part_size) {
-  // bl.mtx.lock();
-  noir::p2p::part_set_header header_{
-    .total = 1, // 4, // header, last_commit, data, evidence
-  };
-  auto p_set = noir::consensus::part_set::new_part_set_from_header(header_);
-
-  p_set.add_part(noir::consensus::part{
-    .index = 0,
-    .bytes_ = noir::codec::scale::encode(bl.header), // .proof
-  });
-  // TODO: part_size & bl.data
-  // bl.mtx.unlock();
-  return p_set;
-}
-
-inline noir::consensus::commit make_commit(int64_t height, noir::p2p::tstamp timestamp) {
-  static const std::string sig_s("Signature");
-  noir::consensus::commit_sig sig_{
-    .flag = noir::consensus::block_id_flag::FlagCommit,
-    .validator_address = gen_random_bytes(32),
-    .timestamp = timestamp,
-    .signature = {sig_s.begin(), sig_s.end()},
-  };
-  return {
-    .height = height,
-    .my_block_id = {.hash = gen_random_bytes(32),
-      .parts =
-        {
-          .total = 2,
-          .hash = gen_random_bytes(32),
-        }},
-    .signatures = {sig_},
-  };
-}
-
 inline noir::consensus::state make_genesis_state() {
   auto config_ = noir::consensus::config::get_default();
   config_.base.chain_id = "test_block_store";
@@ -96,7 +43,7 @@ TEST_CASE("save/load_block", "[block_store]") {
     auto bl_ = make_block(height, genesis_state, commit_);
     auto hash_ = bl_.get_hash(); // TODO: temporary workaround (block get_hash() not implemented)
     auto p_set_ = make_part_set(bl_, 2);
-    auto seen_commit_ = make_commit(10, noir::p2p::tstamp{});
+    auto seen_commit_ = noir::consensus::make_commit(10, noir::p2p::tstamp{});
 
     CHECK(bls.save_block(bl_, p_set_, seen_commit_) == true);
     CHECK(bls.base() == 1ll);
@@ -175,7 +122,7 @@ TEST_CASE("prune_block", "[block_store]") {
   for (auto height = 1; height <= 1500; ++height) {
     auto bl_ = make_block(height, genesis_state, commit_);
     auto p_set_ = make_part_set(bl_, 2);
-    auto seen_commit_ = make_commit(10, noir::p2p::tstamp{});
+    auto seen_commit_ = noir::consensus::make_commit(10, noir::p2p::tstamp{});
 
     CHECK(bls.save_block(bl_, p_set_, seen_commit_) == true);
     CHECK(bls.base() == 1ll);
