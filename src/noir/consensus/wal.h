219d26a103921a398f42a7dd6be2265fbc2fd8ce
xedonman <taxi2se@gmail.com>
2022-02-15 16:56:45 +0900

    refactor: (consensus/wal)
    following changes are made
    - add internal index to manage min/max index
    - devide wal_codec to wal_file_manager+wal_encoder
      - wal_file_manager: rotate wal file and handles mutex
      - wal_encder: encodes msg to wal file

--- src/noir/consensus/wal.h
+++ src/noir/consensus/wal.h
@@ -40,7 +40,7 @@ struct wal_search_options {
 };
 
 /// \brief A WALDecoder reads and decodes custom-encoded WAL messages from an input
-/// stream. See WALCodec for the format used.
+/// stream. See WALEncoder for the format used.
 /// It will also compare the checksums and make sure data size is equal to the
 /// length from the header. If that is not the case, error will be returned.
 class wal_decoder {
@@ -50,8 +50,8 @@ public:
     eof,
     corrupted,
   };
-
-  wal_decoder(const std::string& full_path, std::shared_ptr<std::mutex> mtx_);
+  wal_decoder(const std::string& full_path, std::shared_ptr<std::mutex> mtx);
+  wal_decoder(const std::string& full_path);
 
   /// \brief reads the next custom-encoded value from its reader and returns it.
   /// \param[out] msg decoded timed_wal_message
@@ -59,16 +59,18 @@ public:
   result decode(timed_wal_message& msg);
 
 private:
-  std::unique_ptr<::fc::cfile> file_;
-  std::shared_ptr<std::mutex> mtx;
+  std::shared_ptr<::fc::cfile> file_;
+  std::shared_ptr<std::mutex> mtx_;
 };
 
-/// \brief A WALCodec writes custom-encoded WAL messages to an output stream.
+/// \brief A WALEncoder writes custom-encoded WAL messages to an output stream.
 /// Format: 4 bytes CRC sum + 4 bytes length + arbitrary-length value
-class wal_codec {
+class wal_encoder {
+  friend class wal_file_manager;
+
 public:
-  wal_codec(const std::string& dir, size_t rotate_size = maxMsgSizeBytes, size_t num_file = default_num_file);
-  ~wal_codec();
+  wal_encoder(const std::string& full_path, std::shared_ptr<std::mutex> mtx);
+  wal_encoder(const std::string& full_path);
 
   /// \brief writes the custom encoding of v to the stream. It returns an error if the encoded size of v is
   /// greater than 1MB. Any error encountered during the write is also returned.
@@ -85,22 +87,140 @@ public:
   /// \return size
   size_t size();
 
-  /// \brief gets list of wal file indexes ordered by last modified
-  /// \return vector of index
-  std::vector<int> reverse_file_index();
+private:
+  // FIXME: below constants are not correct
+  static constexpr size_t max_msg_size = 1048576; // 1 MB; NOTE: keep in sync with types.PartSet sizes.
+  static constexpr size_t max_msg_size_bytes = max_msg_size + 24; // time.Time + max consensus msg size
+
+  std::shared_ptr<::fc::cfile> file_;
+  std::shared_ptr<std::mutex> mtx_;
+};
+
+/// \brief WALFileManager manages wal file and mutex
+class wal_file_manager {
+public:
+  wal_file_manager(const std::string& dir, size_t num_file, size_t rotate_size)
+    : dir_path_(dir), num_file_(num_file), rotate_size_(rotate_size), mtx_map_() {
+    auto ret = load_min_max_index();
+    if (!ret) { // no previous wal file found
+      min_index = 0;
+      max_index = 0;
+    }
+    encoder_ = get_wal_encoder(max_index);
+    current_index = max_index;
+  }
+
+  /// \brief rotate wal file if necessary
+  /// \return true on success, false otherwise
+  inline bool update() {
+    if (need_rotate()) {
+      return rotate();
+    }
+    return true;
+  }
+
+  /// \brief gets wal_encoder of wal file of given index
+  /// \param[in] index indicates current_index if the value is -1
+  /// \return shared_ptr of wal_encoder
+  std::shared_ptr<wal_encoder> get_wal_encoder(int64_t index = -1) {
+    if (index < 0 || index == current_index) {
+      return encoder_;
+    }
+    if (mtx_map_[index] == nullptr) {
+      mtx_map_[index] = std::make_shared<std::mutex>();
+    }
+    return make_shared<wal_encoder>(full_path(dir_path_, index).string(), mtx_map_[index]);
+  }
 
   /// \brief gets wal_decoder of wal file of given index
   /// \param[in] index
   /// \return shared_ptr of wal_decoder
-  std::shared_ptr<wal_decoder> new_wal_decoder(int index);
+  std::shared_ptr<wal_decoder> get_wal_decoder(int64_t index) {
+    if (mtx_map_[index] == nullptr) {
+      mtx_map_[index] = std::make_shared<std::mutex>();
+    }
+    return make_shared<wal_decoder>(full_path(dir_path_, index).string(), mtx_map_[index]);
+  }
+
+  /// \brief indicates index of current opened file
+  int64_t current_index = -1;
+  /// \brief indicates lowest index of available WAL file
+  int64_t min_index = -1;
+  /// \brief indicates highest index of available WAL file
+  int64_t max_index = -1;
 
 private:
-  static constexpr size_t maxMsgSize = 1048576; // 1 MB; NOTE: keep in sync with types.PartSet sizes.
-  static constexpr size_t maxMsgSizeBytes = maxMsgSize + 24; // time.Time + max consensus msg size
-  static constexpr size_t default_num_file = 1000; // TODO: find this value
+  static constexpr std::basic_string_view file_name_prefix_{"cs_wal"};
+  std::shared_ptr<wal_encoder> encoder_;
+  std::string dir_path_;
+  size_t num_file_; // TODO: configurable
+  size_t rotate_size_;
+  std::map<int64_t, std::shared_ptr<std::mutex>> mtx_map_;
 
-  std::shared_ptr<std::mutex> mtx;
-  std::unique_ptr<class wal_codec_impl> impl_;
+  static inline fc::path full_path(std::string dir_path, int index) {
+    return {dir_path + "/" + std::string(file_name_prefix_) + std::to_string(index)};
+  }
+
+  static inline int64_t index_from_file_name(const fc::path& file_name) {
+    static constexpr size_t len = file_name_prefix_.length();
+    auto name = file_name.string();
+    if (name.compare(0, len, file_name_prefix_) != 0) {
+      return -1;
+    }
+    return static_cast<int64_t>(std::stoull(name.substr(len)));
+  }
+
+  bool need_rotate() {
+    std::lock_guard<std::mutex> g(*encoder_->mtx_);
+    return encoder_->file_->tellp() >= rotate_size_; // TODO: handle exception
+  }
+
+  bool rotate() {
+    std::lock_guard<std::mutex> g(*encoder_->mtx_);
+    auto& file_ = encoder_->file_;
+    auto new_index = max_index = max_index + 1;
+    auto new_path_ = full_path(dir_path_, new_index);
+    try {
+      file_->flush();
+      file_->sync();
+      file_->close();
+
+      if (new_index - min_index >= num_file_) {
+        fc::remove(full_path(dir_path_, min_index));
+        mtx_map_[min_index] = nullptr;
+        min_index++;
+      }
+      if (fc::exists(new_path_)) {
+        wlog("wal file for new index${index} already exists: ${path}", ("index", new_index)("path", new_path_));
+      }
+      fc::remove(new_path_);
+      encoder_ = get_wal_encoder(new_index);
+      current_index = new_index;
+      ilog("created wal file for new index${index}: ${path}", ("index", new_index)("path", new_path_));
+    } catch (...) {
+      return false;
+    }
+    return true;
+  }
+
+  bool load_min_max_index() {
+    for (auto it = fc::directory_iterator(dir_path_); it != fc::directory_iterator(); ++it) {
+      if (fc::is_regular_file(*it)) {
+        auto index_ = index_from_file_name(it->filename());
+        if (index_ < 0) {
+          continue;
+        }
+        if (min_index < 0 || index_ < min_index) {
+          min_index = index_;
+        }
+        if (max_index < 0 || index_ > max_index) {
+          max_index = index_;
+        }
+      }
+    }
+
+    return (min_index > 0 && max_index > 0);
+  }
 };
 
 /// \brief WAL is an interface for any write-ahead logger.
@@ -148,11 +268,12 @@ public:
 class base_wal : public wal {
 public:
   base_wal(const base_wal&) = delete; // do not allow copy
-  base_wal(const std::string& path, size_t rotate_size, size_t num_file)
-    : codec_(std::make_unique<wal_codec>(path, rotate_size, num_file)),
+  base_wal(const std::string& path, size_t num_file, size_t rotate_size)
+    : file_manager_(std::make_unique<wal_file_manager>(path, num_file, rotate_size)),
       flush_interval(std::chrono::system_clock::duration(2000000)) { // 2seconds = 2000000 microseconds
     thread_pool.emplace("consensus", thread_pool_size);
     {
+      file_manager_->get_wal_encoder();
       // std::lock_guard<std::mutex> g(flush_ticker_mtx);
       flush_ticker.reset(new boost::asio::steady_timer(thread_pool->get_executor()));
     }
@@ -161,7 +282,11 @@ public:
 
   bool write(const wal_message& msg) override {
     size_t len;
-    if (!codec_->encode(timed_wal_message{.time = get_time(), .msg = msg}, len) || len == 0) {
+    if (!file_manager_->update()) {
+      elog("Failed to rotate wal file");
+    }
+    auto enc = file_manager_->get_wal_encoder();
+    if (!enc->encode(timed_wal_message{.time = get_time(), .msg = msg}, len) || len == 0) {
       elog("Error writing msg to consensus wal. WARNING: recover may not be possible for the current height");
       return false;
     }
@@ -182,11 +307,11 @@ public:
   }
 
   bool flush_and_sync() override {
-    return codec_->flush_and_sync();
+    return file_manager_->get_wal_encoder()->flush_and_sync();
   }
 
   bool on_start() override {
-    if (codec_->size() == 0) {
+    if (file_manager_->get_wal_encoder()->size() == 0) {
       end_height_message msg{.height = 0};
       if (write_sync({msg}) == false) {
         return false;
@@ -227,11 +352,18 @@ public:
 
   std::shared_ptr<wal_decoder> search_for_end_height(int64_t height, wal_search_options options, bool& found) override {
     int64_t last_height_found{-1};
+    auto max_index = file_manager_->max_index;
+    auto min_index = file_manager_->min_index;
+    if (min_index < 0 || max_index < 0) {
+      found = false;
+      return {nullptr};
+    }
+    ilog(
+      "Searching for height=${height} min=${min} max=${max}", ("height", height)("min", min_index)("max", max_index));
     // NOTE: starting from the last file in the group because we're usually
     // searching for the last height. See replay.cpp(?)
-    auto f_indexes = codec_->reverse_file_index(); // TODO: index?
-    for (auto it = f_indexes.begin(); it != f_indexes.end(); ++it) {
-      auto decoder = codec_->new_wal_decoder(*it);
+    for (auto index = max_index; index >= min_index; --index) {
+      auto decoder = file_manager_->get_wal_decoder(index);
 
       while (true) { // TODO: check exit condition
         using result = wal_decoder::result;
@@ -273,7 +405,8 @@ public:
   }
 
 private:
-  std::unique_ptr<wal_codec> codec_;
+  std::unique_ptr<wal_file_manager> file_manager_;
+  // flush ticker
   std::unique_ptr<boost::asio::steady_timer> flush_ticker;
   std::chrono::system_clock::duration flush_interval;
   uint16_t thread_pool_size = 2;
--- src/noir/consensus/wal_test.cpp
+++ src/noir/consensus/wal_test.cpp
@@ -21,72 +21,43 @@ int count_dir(const std::string& tmp_path) {
   return cnt;
 }
 
-auto prepare_codec = [](size_t enc_size) {
-  static constexpr size_t rotation_file_size = 5;
-  auto temp_dir = std::make_shared<fc::temp_directory>();
-  auto tmp_path = temp_dir->path().string();
-  REQUIRE(fc::is_directory(tmp_path) == true);
-  REQUIRE(count_dir(tmp_path) == 0);
-
-  return std::tuple<std::shared_ptr<fc::temp_directory>, std::shared_ptr<wal_codec>>(
-    std::move(temp_dir), std::make_shared<wal_codec>(tmp_path, enc_size, rotation_file_size));
-};
-
 TEST_CASE("simple encode/decode", "[wal_codec]") {
   static constexpr size_t enc_size = 0x100;
-  auto [temp_dir, wal_codec_] = prepare_codec(enc_size);
+  auto [temp_dir, wal_manager_] = [](size_t enc_size) {
+    static constexpr size_t rotation_file_num = 5;
+    auto temp_dir = std::make_shared<fc::temp_directory>();
+    auto tmp_path = temp_dir->path().string();
+    REQUIRE(fc::is_directory(tmp_path) == true);
+    REQUIRE(count_dir(tmp_path) == 0);
 
-  int exp = 0;
-  timed_wal_message msg{};
-  size_t tmp;
-  for (size_t size_ = 0; size_ < enc_size; ++exp, size_ += tmp) {
-    auto ret = wal_codec_->encode(msg, tmp);
-    CHECK(ret == true);
-    ++msg.time;
-  }
-  CHECK(wal_codec_->flush_and_sync() == true);
-
-  int count = 0;
-  timed_wal_message ret{.msg = {end_height_message{}}};
-  timed_wal_message exp_msg{};
-  auto decoder = wal_codec_->new_wal_decoder(0);
-  while (count <= exp) {
-    auto res = decoder->decode(ret);
-    CHECK(res != wal_decoder::result::corrupted);
-    if (res == wal_decoder::result::eof) {
-      break;
-    }
-    CHECK(ret.time == exp_msg.time);
-    ++exp_msg.time;
-    ++count;
-  }
-  CHECK(exp == count);
-}
-
-TEST_CASE("encode/decode with file rotation", "[wal_codec]") {
-  static constexpr size_t enc_size = 0x100;
-  auto [temp_dir, wal_codec_] = prepare_codec(enc_size);
+    return std::tuple<std::shared_ptr<fc::temp_directory>, std::shared_ptr<wal_file_manager>>(
+      std::move(temp_dir), std::make_shared<wal_file_manager>(tmp_path, rotation_file_num, enc_size));
+  }(enc_size);
   auto tmp_path = temp_dir->path().string();
+  auto wal_encoder_ = wal_manager_->get_wal_encoder();
+  auto write_msg_to_wal_file = [&wal_manager_ = wal_manager_, &wal_encoder_](
+                                 const timed_wal_message& msg, size_t& len) {
+    wal_manager_->update();
+    wal_encoder_ = wal_manager_->get_wal_encoder();
+    return wal_encoder_->encode(msg, len);
+  };
 
-  timed_wal_message msg{};
-  int exp = 0;
-  for (auto i = 0; i < 5; ++i) {
-    for (size_t size_ = 0; size_ < enc_size;) {
-      size_t tmp;
-      CHECK(wal_codec_->encode(msg, tmp) == true);
-      size_ += tmp;
+  SECTION("simple encode/decode") {
+    int exp = 0;
+    timed_wal_message msg{};
+    size_t tmp;
+
+    for (size_t size_ = 0; size_ < enc_size; ++exp, size_ += tmp) {
+      auto ret = write_msg_to_wal_file(msg, tmp);
+      CHECK(ret == true);
       ++msg.time;
-      exp++;
     }
-  }
-  CHECK(wal_codec_->flush_and_sync() == true);
-  CHECK(count_dir(tmp_path) == 5);
+    CHECK(wal_encoder_->flush_and_sync() == true);
 
-  int count = 0;
-  timed_wal_message exp_msg{};
-  for (auto i = 0; i < 5; ++i) {
-    auto decoder = wal_codec_->new_wal_decoder(i);
+    int count = 0;
     timed_wal_message ret{.msg = {end_height_message{}}};
+    timed_wal_message exp_msg{};
+    auto decoder = wal_manager_->get_wal_decoder(0);
     while (count <= exp) {
       auto res = decoder->decode(ret);
       CHECK(res != wal_decoder::result::corrupted);
@@ -97,35 +68,55 @@ TEST_CASE("encode/decode with file rotation", "[wal_codec]") {
       ++exp_msg.time;
       ++count;
     }
+    CHECK(exp == count);
   }
-  CHECK(exp == count);
 
-  for (auto i = 0; i < 5; ++i) {
-    for (size_t size_ = 0; size_ < enc_size;) {
-      size_t tmp;
-      CHECK(wal_codec_->encode(msg, tmp) == true);
-      size_ += tmp;
+  SECTION("encode/decode with file rotation") {
+    timed_wal_message msg{};
+    int exp = 0;
+    for (auto i = 0; i < 5; ++i) {
+      for (size_t size_ = 0; size_ < enc_size;) {
+        size_t tmp;
+        CHECK(write_msg_to_wal_file(msg, tmp) == true);
+        size_ += tmp;
+        ++msg.time;
+        exp++;
+      }
     }
+    CHECK(wal_encoder_->flush_and_sync() == true);
+    CHECK(count_dir(tmp_path) == 5);
+
+    int count = 0;
+    timed_wal_message exp_msg{};
+    for (auto i = 0; i < 5; ++i) {
+      auto decoder = wal_manager_->get_wal_decoder(i);
+      timed_wal_message ret{.msg = {end_height_message{}}};
+      while (count <= exp) {
+        auto res = decoder->decode(ret);
+        CHECK(res != wal_decoder::result::corrupted);
+        if (res == wal_decoder::result::eof) {
+          break;
+        }
+        CHECK(ret.time == exp_msg.time);
+        ++exp_msg.time;
+        ++count;
+      }
+    }
+    CHECK(exp == count);
+
+    for (auto i = 0; i < 5; ++i) {
+      for (size_t size_ = 0; size_ < enc_size;) {
+        size_t tmp;
+        CHECK(write_msg_to_wal_file(msg, tmp) == true);
+        size_ += tmp;
+      }
+    }
+    CHECK(wal_encoder_->flush_and_sync() == true);
+    CHECK(count_dir(tmp_path) == 5);
+
+    fc::remove_all(tmp_path);
   }
-  CHECK(wal_codec_->flush_and_sync() == true);
-  CHECK(count_dir(tmp_path) == 5);
-
-  fc::remove_all(tmp_path);
 }
-
-auto prepare_wal = [](size_t enc_size) {
-  static constexpr size_t rotation_file_size = 5;
-  auto temp_dir = std::make_shared<fc::temp_directory>();
-  auto tmp_path = temp_dir->path().string();
-  REQUIRE(fc::is_directory(tmp_path) == true);
-  REQUIRE(count_dir(tmp_path) == 0);
-
-  auto wal_ = std::make_shared<base_wal>(tmp_path, enc_size, rotation_file_size);
-  wal_->set_flush_interval(std::chrono::milliseconds(100));
-
-  return std::tuple<std::shared_ptr<fc::temp_directory>, std::shared_ptr<base_wal>>(
-    std::move(temp_dir), std::move(wal_));
-};
 
 inline noir::bytes gen_random_bytes(size_t num) {
   noir::bytes ret(num);
@@ -133,98 +124,109 @@ inline noir::bytes gen_random_bytes(size_t num) {
   return ret;
 }
 
-TEST_CASE("WAL write", "[basic_wal]") {
-  static constexpr size_t enc_size = 0x100;
-  auto [temp_dir, wal_] = prepare_wal(enc_size);
-  wal_->on_start();
-  auto defer = noir::make_scoped_exit([&wal_ = wal_]() { wal_->on_stop(); });
-
-  noir::p2p::block_part_message bp_msg{
-    .height = 1,
-    .round = 1,
-    .index = 1,
-    .bytes_ = gen_random_bytes(32),
-    .proof = gen_random_bytes(32),
-  };
-
-  CHECK(wal_->write({msg_info{.msg = bp_msg}}) == true);
-  CHECK(wal_->flush_and_sync() == true);
-}
-
-TEST_CASE("WAL search_for_end_height", "[basic_wal]") {
+TEST_CASE("basic_wal test", "[basic_wal]") {
   static constexpr size_t enc_size = 4096;
-  auto [temp_dir, wal_] = prepare_wal(enc_size);
+  auto [temp_dir, wal_] = [](size_t enc_size) {
+    static constexpr size_t rotation_file_num = 5;
+    auto temp_dir = std::make_shared<fc::temp_directory>();
+    auto tmp_path = temp_dir->path().string();
+    REQUIRE(fc::is_directory(tmp_path) == true);
+    REQUIRE(count_dir(tmp_path) == 0);
 
-  {
-    int64_t height = 0;
-    bool found;
-    auto dec = wal_->search_for_end_height(height, {.ignore_data_corruption_errors = false}, found);
-    CHECK(found == false);
-    CHECK(dec == nullptr);
-  }
+    auto wal_ = std::make_shared<base_wal>(tmp_path, rotation_file_num, enc_size);
+    wal_->set_flush_interval(std::chrono::milliseconds(100));
 
-  wal_->on_start();
+    return std::tuple<std::shared_ptr<fc::temp_directory>, std::shared_ptr<base_wal>>(
+      std::move(temp_dir), std::move(wal_));
+  }(enc_size);
+  auto tmp_path = temp_dir->path().string();
   auto defer = noir::make_scoped_exit([&wal_ = wal_]() { wal_->on_stop(); });
-  {
-    int64_t height = 0;
-    bool found;
-    // starting wal should write end_height_message with height 0
-    auto dec = wal_->search_for_end_height(height, {.ignore_data_corruption_errors = false}, found);
-    CHECK(found == true);
-    CHECK(dec != nullptr);
+
+  SECTION("WAL write") {
+    noir::p2p::block_part_message bp_msg{
+      .height = 1,
+      .round = 1,
+      .index = 1,
+      .bytes_ = gen_random_bytes(32),
+      .proof = gen_random_bytes(32),
+    };
+
+    wal_->on_start();
+    CHECK(wal_->write({msg_info{.msg = bp_msg}}) == true);
+    CHECK(wal_->flush_and_sync() == true);
   }
 
-  for (int64_t height = 1; height < 50; ++height) {
-    uint32_t index;
-    for (index = 1; index < 10; ++index) {
-      noir::p2p::block_part_message bp_msg{
-        .height = height,
-        .round = 1,
-        .index = index,
-        .bytes_ = gen_random_bytes(32),
-        .proof = gen_random_bytes(32),
-      };
-      CHECK(wal_->write({msg_info{.msg = bp_msg}}) == true);
-    }
-    CHECK(wal_->write({noir::consensus::end_height_message{height}}) == true);
-  }
-  CHECK(wal_->flush_and_sync() == true);
-
-  {
-    int64_t height = 49;
-    bool found;
-    auto dec = wal_->search_for_end_height(height, {.ignore_data_corruption_errors = false}, found);
-    CHECK(found == true);
-    REQUIRE(dec != nullptr);
-    timed_wal_message msg{};
-    CHECK(dec->decode(msg) == wal_decoder::result::eof);
-  }
-  {
-    int64_t height;
-    bool deleted = false;
-    for (height = 48; height >= 0; --height) {
+  SECTION("WAL search_for_end_height") {
+    // initial state
+    {
+      int64_t height = 0;
       bool found;
       auto dec = wal_->search_for_end_height(height, {.ignore_data_corruption_errors = false}, found);
-      if (deleted) {
-        // decoder with deleted height should not be found
-        CHECK(found == false);
+      CHECK(found == false);
+      CHECK(dec == nullptr);
+    }
+    wal_->on_start();
+    // starting wal should write end_height_message with height 0
+    {
+      int64_t height = 0;
+      bool found;
+      auto dec = wal_->search_for_end_height(height, {.ignore_data_corruption_errors = false}, found);
+      CHECK(found == true);
+      CHECK(dec != nullptr);
+    }
+
+    for (int64_t height = 1; height < 50; ++height) {
+      uint32_t index;
+      for (index = 1; index < 10; ++index) {
+        noir::p2p::block_part_message bp_msg{
+          .height = height,
+          .round = 1,
+          .index = index,
+          .bytes_ = gen_random_bytes(32),
+          .proof = gen_random_bytes(32),
+        };
+        CHECK(wal_->write({msg_info{.msg = bp_msg}}) == true);
       }
-      if (!found) {
-        deleted = true;
-        CHECK(dec == nullptr);
-        continue;
-      }
+      CHECK(wal_->write({noir::consensus::end_height_message{height}}) == true);
+    }
+    CHECK(wal_->flush_and_sync() == true);
+
+    {
+      int64_t height = 49;
+      bool found;
+      auto dec = wal_->search_for_end_height(height, {.ignore_data_corruption_errors = false}, found);
+      CHECK(found == true);
       REQUIRE(dec != nullptr);
       timed_wal_message msg{};
-      CHECK(dec->decode(msg) == wal_decoder::result::success);
-      auto* msg_body = get_if<msg_info>(&msg.msg.msg);
-      REQUIRE(msg_body != nullptr);
-      auto* bp_msg = get_if<noir::p2p::block_part_message>(&msg_body->msg);
-      REQUIRE(bp_msg != nullptr);
-      CHECK(bp_msg->height == height + 1);
+      CHECK(dec->decode(msg) == wal_decoder::result::eof);
     }
+    {
+      int64_t height;
+      bool deleted = false;
+      for (height = 48; height >= 0; --height) {
+        bool found;
+        auto dec = wal_->search_for_end_height(height, {.ignore_data_corruption_errors = false}, found);
+        if (deleted) {
+          // decoder with deleted height should not be found
+          CHECK(found == false);
+        }
+        if (!found) {
+          deleted = true;
+          CHECK(dec == nullptr);
+          continue;
+        }
+        REQUIRE(dec != nullptr);
+        timed_wal_message msg{};
+        CHECK(dec->decode(msg) == wal_decoder::result::success);
+        auto* msg_body = get_if<msg_info>(&msg.msg.msg);
+        REQUIRE(msg_body != nullptr);
+        auto* bp_msg = get_if<noir::p2p::block_part_message>(&msg_body->msg);
+        REQUIRE(bp_msg != nullptr);
+        CHECK(bp_msg->height == height + 1);
+      }
+    }
+    CHECK(wal_->flush_and_sync() == true);
   }
-  CHECK(wal_->flush_and_sync() == true);
 }
 
 } // namespace
