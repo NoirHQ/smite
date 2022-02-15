f446ee093efc6bf47312658709de361468407cd5
nonvis@gmail.com <nonvis@gmail.com>
2022-02-15 18:42:47 +0800

    feat: add more test cases for bit_array

--- src/noir/consensus/bit_array_test.cpp
+++ src/noir/consensus/bit_array_test.cpp
@@ -11,8 +11,8 @@
 using namespace noir;
 using namespace noir::consensus;
 
-TEST_CASE("Encode bit_array", "[bit_array]") {
-  auto bit_array_ = bit_array::new_bit_array(9);
+TEST_CASE("Encode and decode bit_array", "[bit_array]") {
+  auto bit_array_ = bit_array::new_bit_array(10);
   auto data = codec::scale::encode(*bit_array_);
   // std::cout << "data=" << to_hex(data) << std::endl;
   // std::cout << "org = " << bit_array_->string() << std::endl;
@@ -26,8 +26,39 @@ TEST_CASE("Encode bit_array", "[bit_array]") {
   auto decoded2 = codec::scale::decode<bit_array>(data);
   CHECK(bit_array_->string() == decoded2.string());
 
-  bit_array_->set_index(5, true);
+  bit_array_->set_index(9, true);
   data = codec::scale::encode(*bit_array_);
   auto decoded3 = codec::scale::decode<bit_array>(data);
   CHECK(bit_array_->string() == decoded3.string());
+
+  int TEST_SIZE = 1000;
+  auto sa = bit_array::new_bit_array(TEST_SIZE);
+  for (auto i = 0; i < TEST_SIZE; i++) {
+    sa->set_index(i, i % 3 != 0);
+    auto enc = codec::scale::encode(*sa);
+    auto dec = codec::scale::decode<bit_array>(enc);
+    CHECK(sa->string() == dec.string());
+  }
+}
+
+TEST_CASE("Check to_bytes", "[bit_array]") {
+  auto ba = bit_array::new_bit_array(4);
+  ba->set_index(3, true);
+  CHECK(ba->get_bytes() == bytes({'\x10'}));
+
+  ba = bit_array::new_bit_array(9);
+  CHECK(ba->get_bytes() == bytes({0, 0}));
+  ba->set_index(7, true);
+  CHECK(ba->get_bytes() == bytes({1, 0}));
+  ba->set_index(8, true);
+  CHECK(ba->get_bytes() == bytes({1, '\x80'}));
+
+  ba = bit_array::new_bit_array(16);
+  CHECK(ba->get_bytes() == bytes({0, 0}));
+  ba->set_index(7, true);
+  CHECK(ba->get_bytes() == bytes({1, 0}));
+  ba->set_index(8, true);
+  CHECK(ba->get_bytes() == bytes({1, '\x80'}));
+  ba->set_index(9, true);
+  CHECK(ba->get_bytes() == bytes({1, '\xC0'}));
 }
