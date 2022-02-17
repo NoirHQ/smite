9480dbf7ed40e6bf5ddeee66f76f1a4d37c98dc9
nonvis@gmail.com <nonvis@gmail.com>
2022-02-17 23:22:46 +0800

    refactor(consensus): properly initialize new_default_node

--- src/noir/consensus/node.h
+++ src/noir/consensus/node.h
@@ -6,6 +6,7 @@
 #pragma once
 #include <noir/common/log.h>
 #include <noir/consensus/block_executor.h>
+#include <noir/consensus/common_test.h>
 #include <noir/consensus/consensus_reactor.h>
 #include <noir/consensus/node_key.h>
 #include <noir/consensus/priv_validator.h>
@@ -28,29 +29,30 @@ struct node {
 
   std::shared_ptr<consensus_reactor> cs_reactor{};
 
-  static std::unique_ptr<node> new_default_node() {
+  static std::unique_ptr<node> new_default_node(const std::shared_ptr<config>& new_config) {
     // Load or generate priv - todo
-    priv_validator local_validator_;
-    local_validator_.pub_key_.key = std::vector<char>(from_hex("000000"));
+    auto [gen_doc, priv_vals] = rand_genesis_doc(*new_config, 1, false, 10);
 
     // Load or generate node_key - todo
     auto node_key_ = node_key::gen_node_key();
 
-    //    make_node(local_validator_, node_key_);
+    auto session = std::make_shared<noir::db::session::session<noir::db::session::rocksdb_t>>(make_session(false));
+
+    make_node(new_config, priv_vals[0], node_key_, std::make_shared<genesis_doc>(gen_doc), session);
   }
 
   static std::unique_ptr<node> make_node(const std::shared_ptr<config>& new_config,
     const priv_validator& new_priv_validator, const node_key& new_node_key,
-    const std::shared_ptr<genesis_doc>& new_genesis_doc) {
+    const std::shared_ptr<genesis_doc>& new_genesis_doc,
+    const std::shared_ptr<noir::db::session::session<noir::db::session::rocksdb_t>>& session) {
 
-    auto session = std::make_shared<noir::db::session::session<noir::db::session::rocksdb_t>>(make_session(false));
     auto dbs = std::make_shared<noir::consensus::db_store>(session);
     auto proxyApp = std::make_shared<app_connection>();
     auto mempool = std::make_shared<tx_pool::tx_pool>();
     auto bls = std::make_shared<noir::consensus::block_store>(session);
     auto block_exec = block_executor::new_block_executor(dbs, proxyApp, mempool, bls);
 
-    state state_ = load_state_from_db_or_genesis(); // todo - properly load state
+    state state_ = load_state_from_db_or_genesis(dbs, new_genesis_doc);
 
     auto cs = consensus_state::new_state(new_config->consensus, state_, block_exec, bls);
 
@@ -89,15 +91,22 @@ struct node {
    * load state from the database, or create one using the given genDoc.
    * On success this returns the genesis doc loaded through the given provider.
    */
-  static state load_state_from_db_or_genesis(/* stateStore, genDoc*/) {
-    // todo
+  static state load_state_from_db_or_genesis(
+    const std::shared_ptr<noir::consensus::db_store>& state_store, const std::shared_ptr<genesis_doc>& gen_doc) {
     // 1. Attempt to load state form the database
-    state state_;
+    state state_{};
+    if (state_store->load(state_)) {
+      dlog("successfully loaded state from db");
+    } else {
+      dlog("unable to load state from db");
+      // return state_; // todo - what to do here?
+    }
 
     if (state_.is_empty()) {
       // 2. If it's not there, derive it from the genesis doc
-      // state_ = state::make_genesis_state();
+      state_ = state::make_genesis_state(*gen_doc);
     }
+
     return state_;
   }
 };
