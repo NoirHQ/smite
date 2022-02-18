e65add4b8267db7a593317a5710b269efcb68185
xedonman <taxi2se@gmail.com>
2022-02-18 19:09:06 +0900

    fix(consensus): change bitsize of blockmeta

--- src/noir/consensus/block_meta.h
+++ src/noir/consensus/block_meta.h
@@ -16,17 +16,17 @@ namespace noir::consensus {
 /// \brief BlockMeta contains meta information.
 struct block_meta {
   noir::p2p::block_id bl_id;
-  int32_t bl_size;
+  int64_t bl_size;
   noir::consensus::block_header header;
-  int32_t num_txs;
+  int64_t num_txs;
   static block_meta new_block_meta(const block& bl_, const part_set& bl_parts) {
     auto hash_ = const_cast<block&>(bl_).get_hash();
     auto parts_ = const_cast<part_set&>(bl_parts);
     return {
       .bl_id = noir::p2p::block_id{.hash{hash_}, .parts{parts_.header()}},
-      .bl_size = static_cast<int32_t>(noir::codec::scale::encode_size(bl_)),
+      .bl_size = static_cast<int64_t>(noir::codec::scale::encode_size(bl_)),
       .header = bl_.header,
-      .num_txs = 0, // TODO: bl_.data.size()
+      .num_txs = static_cast<int64_t>(bl_.data.txs.size()),
     };
   }
 };
