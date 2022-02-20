337fe2f17e190daf48fcabb2505949acf06a4414
nonvis@gmail.com <nonvis@gmail.com>
2022-02-20 16:22:19 +0800

    feat(consensus): add methods to sign vote and proposal and to verify signatures

--- src/noir/consensus/consensus_state_test.cpp
+++ src/noir/consensus/consensus_state_test.cpp
@@ -6,11 +6,12 @@
 #include <catch2/catch_all.hpp>
 #include <noir/consensus/common_test.h>
 
+using namespace noir;
 using namespace noir::consensus;
 
 TEST_CASE("Proposer Selection 0", "[consensus_state]") {
   auto local_config = config_setup();
-  auto [cs1, vss] = rand_cs(local_config, 4);
+  auto [cs1, vss] = rand_cs(local_config, 1);
 
   auto height = cs1->rs.height;
   auto round = cs1->rs.round;
@@ -52,4 +53,46 @@ TEST_CASE("No Priv Validator", "[consensus_state]") {
   force_tick(cs1); // ensureNewRound
 
   CHECK(!cs1->get_round_state()->proposal);
+}
+
+TEST_CASE("Verify proposal signature", "[consensus_state]") {
+  auto local_config = config_setup();
+  auto [cs1, vss] = rand_cs(local_config, 1);
+  auto local_priv_validator = cs1->local_priv_validator.value();
+
+  proposal proposal_{};
+  proposal_.timestamp = get_time();
+
+  auto data_proposal1 = codec::scale::encode((p2p::proposal_message)proposal_);
+  // std::cout << "data_proposal1=" << to_hex(data_proposal1) << std::endl;
+  // std::cout << "digest1=" << fc::sha256::hash(data_proposal1).str() << std::endl;
+  auto sig_org = local_priv_validator.sign_proposal(proposal_);
+  // std::cout << "sig=" << std::string(proposal_.signature.begin(), proposal_.signature.end()) << std::endl;
+
+  auto data_proposal2 = codec::scale::encode((p2p::proposal_message)proposal_);
+  // std::cout << "data_proposal2=" << to_hex(data_proposal2) << std::endl;
+  // std::cout << "digest2=" << fc::sha256::hash(data_proposal2).str() << std::endl;
+  auto result = local_priv_validator.pub_key_.verify_signature(data_proposal2, proposal_.signature);
+  CHECK(result == true);
+}
+
+TEST_CASE("Verify vote signature", "[consensus_state]") {
+  auto local_config = config_setup();
+  auto [cs1, vss] = rand_cs(local_config, 1);
+  auto local_priv_validator = cs1->local_priv_validator.value();
+
+  vote vote_{};
+  vote_.timestamp = get_time();
+
+  auto data_vote1 = codec::scale::encode((p2p::vote_message)vote_);
+  // std::cout << "data_vote1=" << to_hex(data_vote1) << std::endl;
+  // std::cout << "digest1=" << fc::sha256::hash(data_vote1).str() << std::endl;
+  auto sig_org = local_priv_validator.sign_vote(vote_);
+  // std::cout << "sig=" << std::string(vote_.signature.begin(), vote_.signature.end()) << std::endl;
+
+  auto data_vote2 = codec::scale::encode((p2p::vote_message)vote_);
+  // std::cout << "data_vote2=" << to_hex(data_vote2) << std::endl;
+  // std::cout << "digest2=" << fc::sha256::hash(data_vote2).str() << std::endl;
+  auto result = local_priv_validator.pub_key_.verify_signature(data_vote2, vote_.signature);
+  CHECK(result == true);
 }
---
+++ src/noir/consensus/vote.cpp
@@ -0,0 +1,164 @@
+// This file is part of NOIR.
+//
+// Copyright (c) 2022 Haderech Pte. Ltd.
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+#include <noir/codec/scale.h>
+#include <noir/consensus/bit_array.h>
+#include <noir/consensus/vote.h>
+
+namespace noir::consensus {
+
+std::shared_ptr<vote_set> vote_set::new_vote_set(std::string& chain_id_, int64_t height_, int32_t round_,
+  p2p::signed_msg_type signed_msg_type, validator_set& val_set_) {
+  if (height_ == 0)
+    throw std::runtime_error("Cannot make vote_set for height = 0, doesn't make sense");
+  auto ret = std::make_shared<vote_set>();
+  ret->chain_id = chain_id_;
+  ret->height = height_;
+  ret->round = round_;
+  ret->signed_msg_type_ = signed_msg_type;
+  ret->val_set = val_set_;
+  ret->votes_bit_array = bit_array::new_bit_array(val_set_.size());
+  ret->sum = 0;
+  return ret;
+}
+
+std::shared_ptr<bit_array> vote_set::get_bit_array() {
+  std::lock_guard<std::mutex> g(mtx);
+  return votes_bit_array->copy();
+}
+
+bool vote_set::add_vote(std::optional<vote> vote_) {
+  if (!vote_.has_value())
+    throw std::runtime_error("add_vote() on empty vote_set");
+
+  std::lock_guard<std::mutex> g(mtx);
+
+  auto val_index = vote_->validator_index;
+  auto val_addr = vote_->validator_address;
+  auto block_key = vote_->block_id_.key();
+
+  // Ensure that validator index is set
+  if (val_index < 0) {
+    elog("add_vote(): validator index < 0");
+    return false;
+  }
+  if (val_addr.empty()) {
+    elog("add_vote(): invalid validator address");
+    return false;
+  }
+
+  // Make sure step matches
+  if ((vote_->height != height) || (vote_->round != round) || (vote_->type != signed_msg_type_)) {
+    elog(fmt::format(
+      "expected {}/{}/{} but got {}/{}/{}", vote_->height, vote_->round, vote_->type, height, round, signed_msg_type_));
+    return false;
+  }
+
+  // Ensure that signer is a validator
+  auto val = val_set.get_by_index(val_index);
+  if (!val.has_value()) {
+    elog(fmt::format("cannot find validator {} in val_set of size {}", val_index, val_set.validators.size()));
+    return false;
+  }
+
+  // Ensure that signer has the right address
+  if (val_addr != val->address) {
+    elog("add_vote() failed: signer has wrong addres");
+    return false;
+  }
+
+  // Check if the same vote exists
+  if (auto existing = get_vote(val_index, block_key); existing.has_value()) {
+    if (existing->signature == vote_->signature) {
+      // duplicate
+      return false;
+    }
+    elog("add_vote() failed: same vote exists");
+    return false;
+  }
+
+  // Check signature
+  if (val->pub_key_.address() != val_addr) {
+    elog("add_vote() failed: invalid validator address");
+    return false;
+  }
+  p2p::vote_message temp(vote_.value());
+  temp.signature = {}; // TODO: debug why manual reset is required for serialization
+  // auto data_vote = codec::scale::encode(static_cast<p2p::vote_message>(vote_.value()));
+  auto data_vote = codec::scale::encode(temp);
+  if (!val->pub_key_.verify_signature(data_vote, vote_->signature)) {
+    elog("add_vote() failed: invalid signature");
+    return false;
+  }
+
+  // Add vote and get conflicting vote if any
+  auto voting_power = val->voting_power;
+  std::optional<vote> conflicting;
+
+  // Already exists in vote_set.votes?
+  if (votes.size() > 0 && votes.size() >= val_index) {
+    auto existing = votes[val_index];
+    if (existing.block_id_ == vote_->block_id_) {
+      throw std::runtime_error("add_vote() does not expect duplicate votes");
+    } else {
+      conflicting = existing;
+    }
+    // Replace vote if block_key matches vote_set.maj23
+    if (maj23.has_value() && maj23.value().key() == block_key) {
+      votes[val_index] = vote_.value();
+      votes_bit_array->set_index(val_index, true);
+    }
+    // Otherwise, don't add to vote_set.votes
+  } else {
+    // Add to vote_set.votes and increase sum
+    votes.push_back(vote_.value());
+    votes_bit_array->set_index(val_index, true);
+    sum += voting_power;
+  }
+
+  block_votes new_votes_by_block;
+  if (votes_by_block.contains(block_key)) {
+    new_votes_by_block = votes_by_block[block_key];
+    if (conflicting.has_value() && votes_by_block[block_key].peer_maj23) {
+      // There's a conflict and no peer claims that this block is special.
+      return false;
+    }
+    // We'll add the vote in a bit.
+  } else {
+    if (conflicting.has_value()) {
+      // there's a conflicting vote.
+      // We're not even tracking this blockKey, so just forget it.
+      return false;
+    }
+    // Start tracking this blockKey
+    new_votes_by_block = block_votes::new_block_votes(false, val_set.size());
+    votes_by_block[block_key] = new_votes_by_block;
+    // We'll add the vote in a bit.
+  }
+
+  // Before adding to votesByBlock, see if we'll exceed quorum
+  auto orig_sum = new_votes_by_block.sum;
+  auto quorum = val_set.get_total_voting_power() * 2 / 3 + 1;
+
+  // Add vote to votesByBlock
+  new_votes_by_block.add_verified_vote(vote_.value(), voting_power);
+
+  // If we just crossed the quorum threshold and have 2/3 majority...
+  if (orig_sum < quorum && quorum <= new_votes_by_block.sum) {
+    // Only consider the first quorum reached
+    if (!maj23.has_value()) {
+      auto maj23_block_id = vote_.value().block_id_;
+      maj23 = maj23_block_id;
+      // And also copy votes over to voteSet.votes
+      for (auto i = 0; const auto& v : new_votes_by_block.votes) {
+        votes[i++] = v;
+      }
+    }
+  }
+
+  return true;
+}
+
+} // namespace noir::consensus
