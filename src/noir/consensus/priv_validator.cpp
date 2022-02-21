8b137f66c89014ac857f06d900f647bc003b90e2
nonvis@gmail.com <nonvis@gmail.com>
2022-02-21 12:28:51 +0800

    reformat(consensus): apply correct use of for_each_field for derived structs

--- src/noir/consensus/priv_validator.cpp
+++ src/noir/consensus/priv_validator.cpp
@@ -3,16 +3,16 @@
 // Copyright (c) 2022 Haderech Pte. Ltd.
 // SPDX-License-Identifier: AGPL-3.0-or-later
 //
-#include <noir/codec/scale.h>
 #include <noir/consensus/priv_validator.h>
+
+#include <noir/codec/scale.h>
 
 namespace noir::consensus {
 
 std::optional<std::string> priv_validator::sign_vote(vote& vote_) {
   // TODO: add some validation checks
 
-  // auto bz = codec::scale::encode(vote_);
-  auto bz = codec::scale::encode(static_cast<p2p::vote_message>(vote_));
+  auto bz = codec::scale::encode(vote_);
   auto sig = priv_key_.sign(bz);
   // TODO: save_signed; why?
   vote_.signature = sig;
@@ -22,8 +22,7 @@ std::optional<std::string> priv_validator::sign_vote(vote& vote_) {
 std::optional<std::string> priv_validator::sign_proposal(proposal& proposal_) {
   // TODO: add some validation checks
 
-  // auto bz = codec::scale::encode(proposal_);
-  auto bz = codec::scale::encode(static_cast<p2p::proposal_message>(proposal_));
+  auto bz = codec::scale::encode(proposal_);
   auto sig = priv_key_.sign(bz);
   // TODO: save_signed; why?
   proposal_.signature = sig;
