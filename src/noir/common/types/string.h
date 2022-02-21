914e412f3d39af140b76b94a05ce2cecf756eee6
Jeeyong Um <conr2d@gmail.com>
2022-02-22 02:40:07 +0800

    style: workaround fix for invalid clang-format correction

--- src/noir/common/types/string.h
+++ src/noir/common/types/string.h
@@ -13,12 +13,13 @@ std::string to_string(T&& v) {
   constexpr bool has_to_string = requires(const T& t) {
     t.to_string();
   };
+  // clang-format off
   if constexpr (has_to_string) {
     return v.to_string();
-  }
-  else {
+  } else {
     return std::to_string(v);
   }
+  // clang-format on
 }
 
 } // namespace noir
