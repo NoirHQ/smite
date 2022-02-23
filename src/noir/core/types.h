9cefc27a584b2e82e848a092e0791a5390e90f75
nonvis@gmail.com <nonvis@gmail.com>
2022-02-24 00:15:18 +0800

    refactor(p2p): use namespace aliases to switch amongst supported codecs

---
+++ src/noir/core/types.h
@@ -0,0 +1,13 @@
+// This file is part of NOIR.
+//
+// Copyright (c) 2022 Haderech Pte. Ltd.
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+#pragma once
+#include <noir/codec/scale.h>
+
+namespace noir::core {
+
+namespace codec = noir::codec::scale;
+
+}
