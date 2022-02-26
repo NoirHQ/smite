22dd1b4a7a16529b3e31c091cbc7099ae22a97cc
nonvis@gmail.com <nonvis@gmail.com>
2022-02-26 15:12:33 +0800

    feat(consensus): add peer_round_state

--- src/noir/consensus/peer_state.h
+++ src/noir/consensus/peer_state.h
@@ -7,14 +7,24 @@
 #include <noir/consensus/node_id.h>
 #include <noir/consensus/types.h>
 
+#include <utility>
+
 namespace noir::consensus {
 
 struct peer_state {
   node_id peer_id;
 
   std::mutex mtx;
-  bool is_running;
-  // peer_round_state prs; // todo
+  bool is_running{};
+  peer_round_state prs;
+
+  static std::shared_ptr<peer_state> new_peer_state(node_id peer_id_) {
+    auto ret = std::make_shared<peer_state>();
+    ret->peer_id = std::move(peer_id_);
+    peer_round_state prs_;
+    ret->prs = prs_;
+    return ret;
+  }
 };
 
 } // namespace noir::consensus
