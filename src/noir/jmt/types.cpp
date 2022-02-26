6e2bb50c00a8e8207db72293347960a7cd824135
Jeeyong Um <conr2d@gmail.com>
2022-02-27 00:52:34 +0800

    feat(bytesN): add to_(bitset | variable-length span)

--- src/noir/jmt/types.cpp
+++ src/noir/jmt/types.cpp
@@ -3,24 +3,14 @@
 // Copyright (c) 2022 Haderech Pte. Ltd.
 // SPDX-License-Identifier: AGPL-3.0-or-later
 //
+#include <noir/common/types/bytes.h>
 #include <noir/jmt/types/common.h>
-#include <bitset>
-#include <numeric>
 
 namespace noir::jmt {
 
-template<size_t N, typename T>
-std::bitset<8 * N> to_bitset(const bytesN<N, T>& bytes) {
-  std::bitset<8 * N> out;
-  return std::accumulate(bytes.begin(), bytes.end(), out, [](auto out, const auto& v) {
-    decltype(out) current(v);
-    return (std::move(out) << 8) | current;
-  });
-}
-
 size_t common_prefix_bits_len(const bytes32& a, const bytes32& b) {
-  auto a_bits = to_bitset(a);
-  auto b_bits = to_bitset(b);
+  auto a_bits = a.to_bitset();
+  auto b_bits = b.to_bitset();
   auto a_pos = 0;
   for (auto b_pos = 0; a_pos < 256 && b_pos < 256; ++a_pos, ++b_pos) {
     if (a_bits[a_pos] == b_bits[b_pos])
