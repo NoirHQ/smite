a06c2629257a2c08a097dd62d4cd3c3283c758bd
xedonman <taxi2se@gmail.com>
2022-03-02 23:03:20 +0900

    feat(priv_val): add file_priv_val ctor/save//load

--- src/noir/consensus/privval/test/file_test.cpp
+++ src/noir/consensus/privval/test/file_test.cpp
@@ -28,6 +28,21 @@ inline noir::bytes gen_random_bytes(size_t num) {
   return ret;
 }
 
+auto compare_file_pv_key = [](const file_pv_key& exp, const file_pv_key& ret) {
+  CHECK(exp.address == ret.address);
+  CHECK(exp.pub_key == ret.pub_key);
+  CHECK(exp.priv_key == ret.priv_key);
+  CHECK(exp.file_path == ret.file_path);
+};
+
+auto compare_file_pv_last_sign_state = [](const file_pv_last_sign_state& exp, const file_pv_last_sign_state& ret) {
+  CHECK(exp.height == ret.height);
+  CHECK(exp.round == ret.round);
+  CHECK(exp.step == ret.step);
+  CHECK(exp.signature == ret.signature);
+  CHECK(exp.file_path == ret.file_path);
+};
+
 TEST_CASE("priv_val_file: save/load priv_key", "[noir][consensus]") {
   auto temp_dir = prepare_test_dir();
   auto temp_dir_path = temp_dir->path().string();
@@ -53,9 +68,7 @@ TEST_CASE("priv_val_file: save/load priv_key", "[noir][consensus]") {
   file_pv_key ret{};
   file_pv_key::load(json_file_path, ret);
 
-  CHECK(exp.address == ret.address);
-  CHECK(exp.pub_key == ret.pub_key);
-  CHECK(exp.priv_key == ret.priv_key);
+  compare_file_pv_key(exp, ret);
 }
 
 TEST_CASE("priv_val_file: save/load lss", "[noir][consensus]") {
@@ -79,11 +92,72 @@ TEST_CASE("priv_val_file: save/load lss", "[noir][consensus]") {
   file_pv_last_sign_state ret{};
   file_pv_last_sign_state::load(json_file_path, ret);
 
-  CHECK(exp.height == ret.height);
-  CHECK(exp.round == ret.round);
-  CHECK(exp.step == ret.step);
-  CHECK(exp.signature == ret.signature);
-  CHECK(exp.sign_bytes == ret.sign_bytes);
+  compare_file_pv_last_sign_state(exp, ret);
+}
+
+TEST_CASE("test file_pv", "[priv_val_file]") {
+  auto temp_dir = prepare_test_dir();
+  auto temp_dir_path = temp_dir->path().string();
+  auto defer = noir::make_scope_exit([&temp_dir_path]() { fs::remove_all(temp_dir_path); });
+
+  auto key_file_path = fs::path{temp_dir_path};
+  key_file_path /= "priv_validator_key";
+  key_file_path.replace_extension("json");
+
+  auto lss_file_path = fs::path{temp_dir_path};
+  lss_file_path /= "priv_validator_state";
+  lss_file_path.replace_extension("json");
+
+  struct test_case {
+    std::string name;
+    std::string key_type; // TODO: connect key_type
+  };
+  auto tests = std::to_array<struct test_case>({
+    test_case{"priv key_type: secp256k1", "secp256k1"},
+    test_case{"priv key_type: ed25519", "ed25519"},
+  });
+
+  std::for_each(tests.begin(), tests.end(), [&](const test_case& t) {
+    SECTION(t.name) {
+
+      auto file_pv_ptr = file_pv::gen_file_pv(key_file_path, lss_file_path, "secp256k1");
+      REQUIRE(file_pv_ptr != nullptr);
+
+      SECTION("gen_file_pv") {
+        {
+          auto& file_pv_key_ = file_pv_ptr->key;
+          CHECK(file_pv_key_.pub_key.empty() == false);
+          CHECK(file_pv_key_.priv_key.key.empty() == false);
+          CHECK(file_pv_key_.file_path == key_file_path);
+        }
+        {
+          auto& lss = file_pv_ptr->last_sign_state;
+          CHECK(lss.step == sign_step::none);
+          CHECK(lss.file_path == lss_file_path);
+        }
+      }
+
+      file_pv_ptr->save();
+
+      SECTION("load_file_pv_empty_state") {
+        {
+          auto ret = file_pv::load_file_pv_empty_state(key_file_path, lss_file_path);
+          REQUIRE(ret != nullptr);
+          // load_file_pv_empty_state() does not load last_sign_state
+          compare_file_pv_key(ret->key, file_pv_ptr->key);
+        }
+      }
+
+      SECTION("load_file_pv") {
+        {
+          auto ret = file_pv::load_file_pv(key_file_path, lss_file_path);
+          REQUIRE(ret != nullptr);
+          compare_file_pv_key(ret->key, file_pv_ptr->key);
+          compare_file_pv_last_sign_state(ret->last_sign_state, file_pv_ptr->last_sign_state);
+        }
+      }
+    }
+  });
 }
 
 } // namespace
