32ec53ce995a51624bfc734987e08546aa0c9594
Jeeyong Um <conr2d@gmail.com>
2022-03-11 10:53:42 +0800

    feat: add hex to integer conversion functions

--- src/noir/common/hex.h
+++ src/noir/common/hex.h
@@ -4,6 +4,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 //
 #pragma once
+#include <noir/common/types/inttypes.h>
 #include <span>
 #include <vector>
 #include <string_view>
@@ -17,11 +18,25 @@ namespace noir {
 /// \param s sequence of bytes
 std::string to_hex(std::span<const char> s);
 
+std::string to_hex(uint8_t v);
+std::string to_hex(uint16_t v);
+std::string to_hex(uint32_t v);
+std::string to_hex(uint64_t v);
+std::string to_hex(uint128_t v);
+std::string to_hex(uint256_t v);
+
 /// \brief converts hex string to bytes
 /// \param s hex string
 /// \param out output buffer
 /// \return size of bytes written
 size_t from_hex(std::string_view s, std::span<char> out);
+
+void from_hex(std::string_view s, uint8_t& v);
+void from_hex(std::string_view s, uint16_t& v);
+void from_hex(std::string_view s, uint32_t& v);
+void from_hex(std::string_view s, uint64_t& v);
+void from_hex(std::string_view s, uint128_t& v);
+void from_hex(std::string_view s, uint256_t& v);
 
 /// \brief converts hex string to bytes
 /// \return byte sequence
---
+++ src/noir/common/test/hex_test.cpp
@@ -0,0 +1,88 @@
+// This file is part of NOIR.
+//
+// Copyright (c) 2022 Haderech Pte. Ltd.
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+#include <catch2/catch_all.hpp>
+#include <noir/common/hex.h>
+
+using namespace noir;
+
+template<typename TestType, size_t N>
+void test_hex_conversion(const std::array<std::pair<TestType, const char*>, N>& tests) {
+  std::for_each(tests.begin(), tests.end(), [](const auto& t) {
+    TestType v = 0;
+    from_hex(t.second, v);
+    CHECK(t.first == v);
+    CHECK(to_hex(t.first) == t.second);
+  });
+}
+
+TEST_CASE("hex", "[noir][common]") {
+  SECTION("uint8_t") {
+    auto tests = std::to_array<std::pair<uint8_t, const char*>>({
+      {0, "00"},
+      {1, "01"},
+      {42, "2a"},
+      {128, "80"},
+      {255, "ff"},
+    });
+
+    test_hex_conversion(tests);
+  }
+
+  SECTION("uint16_t") {
+    auto tests = std::to_array<std::pair<uint16_t, const char*>>({
+      {0, "0000"},
+      {1, "0001"},
+      {32767, "7fff"},
+      {32768, "8000"},
+      {65535, "ffff"},
+    });
+
+    test_hex_conversion(tests);
+  }
+
+  SECTION("uint32_t") {
+    auto tests = std::to_array<std::pair<uint32_t, const char*>>({
+      {0, "00000000"},
+      {1, "00000001"},
+      {2'147'483'647, "7fffffff"},
+      {2'147'483'648, "80000000"},
+      {4'294'967'295, "ffffffff"},
+    });
+
+    test_hex_conversion(tests);
+  }
+
+  SECTION("uint64_t") {
+    auto tests = std::to_array<std::pair<uint64_t, const char*>>({
+      {0, "0000000000000000"},
+      {1, "0000000000000001"},
+      {9'223'372'036'854'775'807ull, "7fffffffffffffff"},
+      {9'223'372'036'854'775'808ull, "8000000000000000"},
+      {18'446'744'073'709'551'615ull, "ffffffffffffffff"},
+    });
+
+    test_hex_conversion(tests);
+  }
+
+  SECTION("uint128_t") {
+    uint128_t med = (uint128_t)uint64_t(-1) + 1;
+    uint128_t max = (uint128_t)(uint64_t(-1)) << 64 | uint64_t(-1);
+
+    auto tests = std::to_array<std::pair<uint128_t, const char*>>({
+      {0, "00000000000000000000000000000000"},
+      {1, "00000000000000000000000000000001"},
+      {med, "00000000000000010000000000000000"},
+      {max, "ffffffffffffffffffffffffffffffff"},
+    });
+
+    test_hex_conversion(tests);
+  }
+
+  SECTION("uint256_t") {
+    auto v = uint256_t("0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141");
+    CHECK(to_hex(v) == "fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141");
+  }
+}
