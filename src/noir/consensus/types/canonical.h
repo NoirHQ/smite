68176f4f76af2f35a9021caeb92f15510f5a2916
nonvis@gmail.com <nonvis@gmail.com>
2022-03-14 00:09:45 +0800

    feat(consensus): add canonical to properly prepare proposal and vote messages for generating and verifying signatures

---
+++ src/noir/consensus/types/canonical.h
@@ -0,0 +1,53 @@
+// This file is part of NOIR.
+//
+// Copyright (c) 2022 Haderech Pte. Ltd.
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+#pragma once
+#include <noir/consensus/vote.h>
+
+namespace noir::consensus {
+
+struct canonical_vote {
+  p2p::signed_msg_type type;
+  int64_t height{};
+  int32_t round{};
+  p2p::block_id block_id_;
+  p2p::tstamp timestamp{};
+  p2p::vote_extension vote_extension_;
+};
+
+struct canonical_proposal {
+  p2p::signed_msg_type type;
+  int64_t height{};
+  int32_t round{};
+  int32_t pol_round{};
+  p2p::block_id block_id_;
+  p2p::tstamp timestamp{};
+};
+
+struct canonical {
+
+  static canonical_vote canonicalize_vote(const vote& vote_) {
+    return {.type = vote_.type,
+      .height = vote_.height,
+      .round = vote_.round,
+      .block_id_ = vote_.block_id_,
+      .timestamp = vote_.timestamp,
+      .vote_extension_ = vote_.vote_extension_};
+  }
+
+  static canonical_proposal canonicalize_proposal(const p2p::proposal_message& proposal_) {
+    return {.type = proposal_.type,
+      .height = proposal_.height,
+      .round = proposal_.round,
+      .pol_round = proposal_.pol_round,
+      .block_id_ = proposal_.block_id_,
+      .timestamp = proposal_.timestamp};
+  }
+};
+
+} // namespace noir::consensus
+
+NOIR_REFLECT(noir::consensus::canonical_vote, type, height, round, block_id_, timestamp, vote_extension_)
+NOIR_REFLECT(noir::consensus::canonical_proposal, type, height, round, pol_round, block_id_, timestamp)
