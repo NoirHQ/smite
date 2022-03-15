bdcce3c7a675d84c1a93a805da1b607488a4b7c6
nonvis@gmail.com <nonvis@gmail.com>
2022-03-15 18:35:00 +0800

    fix(consensus): use canonicalize for vote and proposal before signing them

--- src/noir/consensus/abci.h
+++ src/noir/consensus/abci.h
@@ -49,6 +49,7 @@ public:
       // Start non-validator node; aka full node (but not seed node) TODO: do we need to support seed nodes?
       appbase::app().set_home_dir("/tmp");
       config_->consensus.root_dir = "/tmp";
+      config_->priv_validator.root_dir = "/tmp";
       config_->base.mode = Full;
     }
 
