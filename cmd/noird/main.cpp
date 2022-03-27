28f48959528048dda6ba7290141e5417fcccb01c
xedonman <taxi2se@gmail.com>
2022-03-27 16:42:42 +0800

    feat(common): print backtrace on segv

--- cmd/noird/main.cpp
+++ cmd/noird/main.cpp
@@ -4,18 +4,21 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 //
 #include <noir/commands/commands.h>
+#include <noir/common/backtrace.h>
 #include <noir/common/log.h>
 #include <noir/rpc/jsonrpc.h>
 #include <noir/rpc/rpc.h>
 #include <noir/tx_pool/tx_pool.h>
 #include <appbase/application.hpp>
 #include <eth/rpc/rpc.h>
+#include <signal.h>
 
 using namespace noir;
 
 appbase::application app;
 
 int main(int argc, char** argv) {
+  signal(SIGSEGV, noir::print_backtrace); // install our handler
 
   // set default configuration
   app.config().require_subcommand();
