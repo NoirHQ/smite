072be95a3e1069a001a70349b134376905a7227d
Jeeyong Um <conr2d@gmail.com>
2022-03-28 22:19:49 +0800

    feat(tm/abci): continue implementing socket client

--- src/tendermint/abci/client/socket_client.cpp
+++ src/tendermint/abci/client/socket_client.cpp
@@ -9,8 +9,19 @@
 namespace tendermint::abci {
 
 SocketClient::SocketClient(appbase::application& app, const std::string& addr, bool must_connect)
-  : Client(app), addr(addr), must_connect(must_connect), thread_pool("socketClient", 4 /* make configurable */) {
-  name = "socketClient";
+  : Client(app), addr(addr), must_connect(must_connect), thread_pool("SocketClient", 4 /* make configurable */) {
+  name = "SocketClient";
+}
+
+result<void> SocketClient::on_start() noexcept {
+  return {};
+}
+
+void SocketClient::on_stop() noexcept {
+}
+
+result<void> SocketClient::on_reset() noexcept {
+  return make_unexpected("not implemented");
 }
 
 void SocketClient::on_set_response_callback(Callback cb) noexcept {
@@ -20,67 +31,70 @@ void SocketClient::on_set_response_callback(Callback cb) noexcept {
 
 result<void> SocketClient::on_error() noexcept {
   std::scoped_lock _(mtx);
-  return make_unexpected(err);
+  if (err) {
+    return make_unexpected(*err);
+  }
+  return {};
 }
 
-result<std::unique_ptr<ReqRes>> SocketClient::on_echo_async(const std::string& msg) noexcept {
+result<std::shared_ptr<ReqRes>> SocketClient::on_echo_async(const std::string& msg) noexcept {
   return queue_request(to_request_echo(msg));
 }
 
-result<std::unique_ptr<ReqRes>> SocketClient::on_flush_async() noexcept {
+result<std::shared_ptr<ReqRes>> SocketClient::on_flush_async() noexcept {
   return queue_request(to_request_flush());
 }
 
-result<std::unique_ptr<ReqRes>> SocketClient::on_info_async(const RequestInfo& req) noexcept {
+result<std::shared_ptr<ReqRes>> SocketClient::on_info_async(const RequestInfo& req) noexcept {
   return queue_request(to_request_info(req));
 }
 
-result<std::unique_ptr<ReqRes>> SocketClient::on_set_option_async(const RequestSetOption& req) noexcept {
+result<std::shared_ptr<ReqRes>> SocketClient::on_set_option_async(const RequestSetOption& req) noexcept {
   return queue_request(to_request_set_option(req));
 }
 
-result<std::unique_ptr<ReqRes>> SocketClient::on_deliver_tx_async(const RequestDeliverTx& req) noexcept {
+result<std::shared_ptr<ReqRes>> SocketClient::on_deliver_tx_async(const RequestDeliverTx& req) noexcept {
   return queue_request(to_request_deliver_tx(req));
 }
 
-result<std::unique_ptr<ReqRes>> SocketClient::on_check_tx_async(const RequestCheckTx& req) noexcept {
+result<std::shared_ptr<ReqRes>> SocketClient::on_check_tx_async(const RequestCheckTx& req) noexcept {
   return queue_request(to_request_check_tx(req));
 }
 
-result<std::unique_ptr<ReqRes>> SocketClient::on_query_async(const RequestQuery& req) noexcept {
+result<std::shared_ptr<ReqRes>> SocketClient::on_query_async(const RequestQuery& req) noexcept {
   return queue_request(to_request_query(req));
 }
 
-result<std::unique_ptr<ReqRes>> SocketClient::on_commit_async(const RequestCommit& req) noexcept {
+result<std::shared_ptr<ReqRes>> SocketClient::on_commit_async(const RequestCommit& req) noexcept {
   return queue_request(to_request_commit(req));
 }
 
-result<std::unique_ptr<ReqRes>> SocketClient::on_init_chain_async(const RequestInitChain& req) noexcept {
+result<std::shared_ptr<ReqRes>> SocketClient::on_init_chain_async(const RequestInitChain& req) noexcept {
   return queue_request(to_request_init_chain(req));
 }
 
-result<std::unique_ptr<ReqRes>> SocketClient::on_begin_block_async(const RequestBeginBlock& req) noexcept {
+result<std::shared_ptr<ReqRes>> SocketClient::on_begin_block_async(const RequestBeginBlock& req) noexcept {
   return queue_request(to_request_begin_block(req));
 }
 
-result<std::unique_ptr<ReqRes>> SocketClient::on_end_block_async(const RequestEndBlock& req) noexcept {
+result<std::shared_ptr<ReqRes>> SocketClient::on_end_block_async(const RequestEndBlock& req) noexcept {
   return queue_request(to_request_end_block(req));
 }
 
-result<std::unique_ptr<ReqRes>> SocketClient::on_list_snapshots_async(const RequestListSnapshots& req) noexcept {
+result<std::shared_ptr<ReqRes>> SocketClient::on_list_snapshots_async(const RequestListSnapshots& req) noexcept {
   return queue_request(to_request_list_snapshots(req));
 }
 
-result<std::unique_ptr<ReqRes>> SocketClient::on_offer_snapshot_async(const RequestOfferSnapshot& req) noexcept {
+result<std::shared_ptr<ReqRes>> SocketClient::on_offer_snapshot_async(const RequestOfferSnapshot& req) noexcept {
   return queue_request(to_request_offer_snapshot(req));
 }
 
-result<std::unique_ptr<ReqRes>> SocketClient::on_load_snapshot_chunk_async(
+result<std::shared_ptr<ReqRes>> SocketClient::on_load_snapshot_chunk_async(
   const RequestLoadSnapshotChunk& req) noexcept {
   return queue_request(to_request_load_snapshot_chunk(req));
 }
 
-result<std::unique_ptr<ReqRes>> SocketClient::on_apply_snapshot_chunk_async(
+result<std::shared_ptr<ReqRes>> SocketClient::on_apply_snapshot_chunk_async(
   const RequestApplySnapshotChunk& req) noexcept {
   return queue_request(to_request_apply_snapshot_chunk(req));
 }
@@ -267,7 +281,7 @@ result<std::unique_ptr<ResponseApplySnapshotChunk>> SocketClient::on_apply_snaps
   return std::unique_ptr<ResponseApplySnapshotChunk>(reqres->response->release_apply_snapshot_chunk());
 }
 
-std::unique_ptr<ReqRes> SocketClient::queue_request(std::unique_ptr<Request> req) {
+std::shared_ptr<ReqRes> SocketClient::queue_request(std::unique_ptr<Request> req) {
   auto reqres = std::make_unique<ReqRes>();
   reqres->request = std::move(req);
   reqres->future = async_thread_pool(thread_pool.get_executor(), [&]() {});
@@ -277,6 +291,68 @@ std::unique_ptr<ReqRes> SocketClient::queue_request(std::unique_ptr<Request> req
     // flush_timer.set();
   }
   return reqres;
+}
+
+void SocketClient::flush_queue() {
+  std::scoped_lock _(mtx);
+
+  // TODO: mark all in-flight messages as resolved (they will get error())
+  // TODO: mark all queued messages as resolved
+}
+
+void SocketClient::will_send_req(std::shared_ptr<ReqRes> reqres) {
+  std::scoped_lock _(mtx);
+  req_sent.push_back(reqres);
+}
+
+bool res_matches_req(Request* req, Response* res) {
+  auto reqv = req->value_case();
+  auto resv = res->value_case();
+  if (!reqv || !resv) {
+    return false;
+  }
+  return reqv + 1 == resv;
+}
+
+result<void> SocketClient::did_recv_response(std::unique_ptr<Response> res) {
+  std::scoped_lock _(mtx);
+
+  if (!req_sent.empty()) {
+    return errorf("unexpected {} when nothing expected", res->GetTypeName());
+  }
+
+  auto reqres = req_sent.front();
+  if (!res_matches_req(reqres->request.get(), res.get())) {
+    return errorf("unexpected {} when response to {} expected", res->GetTypeName(), reqres->request->GetTypeName());
+  }
+
+  reqres->response = std::move(res);
+  // FIXME: need to end async task
+  reqres->set_done();
+  req_sent.pop_front();
+
+  if (res_cb) {
+    res_cb(reqres->request.get(), res.get());
+  }
+
+  reqres->invoke_callback();
+  return {};
+}
+
+void SocketClient::stop_for_error(const tendermint::error& err) {
+  if (!is_running()) {
+    return;
+  }
+  {
+    std::scoped_lock _(mtx);
+    if (!this->err) {
+      this->err = err;
+    }
+  }
+  elog("Stopping SocketClient for error: {}", err);
+  if (auto err = stop(); !err) {
+    elog("Error stopping SocketClient: {}", err.error());
+  }
 }
 
 } // namespace tendermint::abci
