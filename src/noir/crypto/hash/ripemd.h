80d194f7207d0d4849e11cd6bc2c2561d7c6a54b
Jeeyong Um <conr2d@gmail.com>
2022-04-21 12:36:44 +0800

    refactor(crypto): remove openssl legacy provider dependency

--- src/noir/crypto/hash/ripemd.h
+++ src/noir/crypto/hash/ripemd.h
@@ -5,19 +5,25 @@
 //
 #pragma once
 #include <noir/crypto/hash/hash.h>
-#include <noir/crypto/openssl/hash.h>
 
 namespace noir::crypto {
 
 /// \brief generates ripemd160 hash
 /// \ingroup crypto
-struct ripemd160 : public hash<ripemd160>, openssl::hash {
+struct ripemd160 : public hash<ripemd160> {
   using crypto::hash<ripemd160>::final;
 
   crypto::hash<ripemd160>& init();
   crypto::hash<ripemd160>& update(std::span<const char> in);
   void final(std::span<char> out);
   std::size_t digest_size() const;
+
+private:
+  uint32_t s[5];
+  unsigned char buf[64];
+  uint64_t bytes = 0;
+
+  bool inited{false};
 };
 
 } // namespace noir::crypto
