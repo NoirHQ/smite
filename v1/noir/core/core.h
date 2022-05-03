1f3a57fe926d967e2c20ea98b721b76df30be931
code0xff <ian.jungyong.um@gmail.com>
2022-05-04 00:16:29 +0800

    refactor: clean up basic header

--- v1/noir/core/core.h
+++ v1/noir/core/core.h
@@ -4,5 +4,18 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 //
 #pragma once
+#include <noir/core/basic.h>
 
-#include <noir/core/result.h>
+#include <noir/core/as_result.h>
+#include <noir/core/channel.h>
+#include <boost/asio/co_spawn.hpp>
+#include <boost/asio/detached.hpp>
+#include <boost/asio/experimental/awaitable_operators.hpp>
+#include <boost/asio/io_context.hpp>
+
+namespace noir {
+
+using namespace boost::asio;
+using namespace boost::asio::experimental::awaitable_operators;
+
+} //namespace noir
