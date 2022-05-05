4645efca6dd055eb7578a6eb9d655cd30b72bdb2
code0xff <ian.jungyong.um@gmail.com>
2022-05-05 14:29:01 +0800

    feat: add unreachable error

--- v1/noir/core/basic_errors.cpp
+++ v1/noir/core/basic_errors.cpp
@@ -3,5 +3,6 @@
 namespace noir {
 
 const Error err_not_implemented = user_error_registry().register_error("not implemented");
+const Error err_unreachable = user_error_registry().register_error("MUST not reach here");
 
 } // namespace noir
