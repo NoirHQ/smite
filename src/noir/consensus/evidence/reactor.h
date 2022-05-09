1b651f311d39dd34d7cfe999571095e7272edf8e
nonvis@gmail.com <nonvis@gmail.com>
2022-05-09 10:44:58 +0800

    feat(consensus): begin implementing evidence

---
+++ src/noir/consensus/evidence/reactor.h
@@ -0,0 +1,50 @@
+// This file is part of NOIR.
+//
+// Copyright (c) 2022 Haderech Pte. Ltd.
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+#pragma once
+#include <noir/common/plugin_interface.h>
+#include <noir/common/thread_pool.h>
+#include <noir/consensus/evidence/evidence_pool.h>
+
+namespace noir::consensus::evidence {
+
+struct reactor {
+
+  appbase::application& app;
+  std::shared_ptr<evidence_pool> pool;
+  std::unique_ptr<named_thread_pool> thread_pool;
+
+  // Receive an envelope from peers [via p2p]
+  plugin_interface::incoming::channels::bs_reactor_message_queue::channel_type::handle bs_reactor_mq_subscription =
+    app.get_channel<plugin_interface::incoming::channels::es_reactor_message_queue>().subscribe(
+      std::bind(&reactor::process_peer_msg, this, std::placeholders::_1));
+
+  // Receive peer_status update from p2p
+  plugin_interface::channels::update_peer_status::channel_type::handle update_peer_status_subscription =
+    app.get_channel<plugin_interface::channels::update_peer_status>().subscribe(
+      std::bind(&reactor::process_peer_update, this, std::placeholders::_1));
+
+  reactor(appbase::application& app)
+    : app(app), thread_pool(std::make_unique<named_thread_pool>("bs_reactor_thread", 3)) {}
+
+  void process_peer_msg(p2p::envelope_ptr info) {}
+
+  void process_peer_update(plugin_interface::peer_status_info_ptr info) {
+    dlog(fmt::format(
+      "[es_reactor] peer update: peer_id={}, status={}", info->peer_id, p2p::peer_status_to_str(info->status)));
+    switch (info->status) {
+    case p2p::peer_status::up: {
+      break;
+    }
+    case p2p::peer_status::down: {
+      break;
+    }
+    default:
+      break;
+    }
+  }
+};
+
+} // namespace noir::consensus::evidence
---
+++ src/noir/consensus/types/evidence.h
@@ -0,0 +1,126 @@
+// This file is part of NOIR.
+//
+// Copyright (c) 2022 Haderech Pte. Ltd.
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+#pragma once
+#include <noir/abci/types.pb.h>
+#include <noir/consensus/types.h>
+#include <noir/types/evidence.pb.h>
+
+#include <google/protobuf/util/time_util.h>
+
+namespace noir::consensus {
+
+struct evidence {
+  virtual std::shared_ptr<::tendermint::abci::Evidence> get_abci() = 0;
+  virtual bytes get_bytes() = 0;
+  virtual bytes get_hash() = 0;
+  virtual int64_t get_height() = 0;
+  virtual std::string get_string() = 0;
+  virtual p2p::tstamp get_time() = 0;
+  virtual result<void> validate_basic() = 0;
+};
+
+struct duplicate_vote_evidence : public evidence {
+  std::shared_ptr<vote> vote_a{};
+  std::shared_ptr<vote> vote_b{};
+  int64_t total_voting_power{};
+  int64_t validator_power{};
+  p2p::tstamp timestamp{};
+
+  result<std::shared_ptr<duplicate_vote_evidence>> new_duplicate_vote_evidence(const std::shared_ptr<vote>& vote1,
+    const std::shared_ptr<vote>& vote2,
+    p2p::tstamp block_time,
+    const std::shared_ptr<validator_set>& val_set) {
+    if (!vote1 || !vote2)
+      return make_unexpected("missing vote");
+    if (!val_set)
+      return make_unexpected("missing validator_set");
+    auto val = val_set->get_by_address(vote1->validator_address);
+    if (!val.has_value())
+      return make_unexpected("validator is not in validator_set");
+    std::shared_ptr<vote> vote_a_, vote_b_;
+    if (vote1->block_id_.key() < vote2->block_id_.key()) {
+      vote_a_ = vote1;
+      vote_b_ = vote2;
+    } else {
+      vote_a_ = vote2;
+      vote_b_ = vote1;
+    }
+    auto ret = std::make_shared<duplicate_vote_evidence>();
+    ret->vote_a = vote_a_;
+    ret->vote_b = vote_b_;
+    ret->total_voting_power = val_set->total_voting_power;
+    ret->validator_power = val->voting_power;
+    ret->timestamp = block_time;
+    return ret;
+  }
+
+  std::shared_ptr<::tendermint::abci::Evidence> get_abci() override {
+    auto ret = std::make_shared<::tendermint::abci::Evidence>();
+    ret->set_type(::tendermint::abci::DUPLICATE_VOTE);
+    auto val = ret->mutable_validator();
+    *val->mutable_address() = std::string(vote_a->validator_address.begin(), vote_a->validator_address.end());
+    val->set_power(validator_power);
+    ret->set_height(vote_a->height);
+    auto ts = ret->mutable_time();
+    *ts = ::google::protobuf::util::TimeUtil::MicrosecondsToTimestamp(timestamp); // TODO: check if this is right
+    ret->set_total_voting_power(total_voting_power);
+    return ret;
+  }
+
+  bytes get_bytes() override {
+    auto pbe = to_proto();
+    bytes ret;
+    pbe->SerializeToArray(ret.data(), pbe->ByteSizeLong());
+    return ret;
+  }
+
+  bytes get_hash() override {
+    return crypto::sha256()(get_bytes());
+  }
+
+  int64_t get_height() override {
+    return vote_a->height;
+  }
+
+  std::string get_string() override {
+    return fmt::format("duplicate_vote_evidence{vote_a, vote_b}");
+  }
+
+  p2p::tstamp get_time() override {
+    return timestamp;
+  }
+
+  result<void> validate_basic() override {
+    if (!vote_a || !vote_b)
+      return make_unexpected("one or both of votes are empty");
+    // if (vote_a->validate_basic)
+    // if (vote_b->validate_basic)
+    if (vote_a->block_id_.key() >= vote_b->block_id_.key())
+      return make_unexpected("duplicate votes in invalid order");
+    return {};
+  }
+
+  std::shared_ptr<::tendermint::types::DuplicateVoteEvidence> to_proto() {
+    auto ret = std::make_shared<::tendermint::types::DuplicateVoteEvidence>();
+    return ret;
+  }
+
+  result<std::shared_ptr<duplicate_vote_evidence>> from_proto(
+    std::shared_ptr<::tendermint::types::DuplicateVoteEvidence> pb) {
+    if (!pb)
+      return make_unexpected("null duplicate vote evidence");
+    auto ret = std::make_shared<duplicate_vote_evidence>();
+    return ret;
+  }
+};
+
+struct evidence_list {
+  std::vector<evidence> list;
+
+  bytes hash() {}
+};
+
+} // namespace noir::consensus
