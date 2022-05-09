196a3943cc476f7ef7fd127a6df870697f4184b0
Jeeyong Um <conr2d@gmail.com>
2022-05-09 14:50:44 +0800

    feat(v1): add formatter for Error

--- v1/noir/core/error.h
+++ v1/noir/core/error.h
@@ -152,3 +152,19 @@ struct hash<noir::Error> {
 };
 
 } // namespace std
+
+namespace fmt {
+
+template<>
+struct formatter<noir::Error> {
+  constexpr auto parse(format_parse_context& ctx) -> decltype(ctx.begin()) {
+    return ctx.begin();
+  }
+
+  template<typename FormatContext>
+  auto format(const noir::Error& err, FormatContext& ctx) -> decltype(ctx.out()) {
+    return format_to(ctx.out(), "{}", err.message());
+  }
+};
+
+} // namespace fmt
