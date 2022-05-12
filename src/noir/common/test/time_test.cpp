4ae74afa2c0926379f0abdd18a89a70e3593f8c5
Inhyeok Lee <lazka33@gmail.com>
2022-05-12 19:35:58 +0900

    test(consensus): fix time test

--- src/noir/common/test/time_test.cpp
+++ src/noir/common/test/time_test.cpp
@@ -10,8 +10,17 @@
 using namespace noir;
 
 TEST_CASE("time", "[noir][common]") {
-  std::vector<const char*> time_str({"2022-03-23T13:23:00Z", "2022-03-23T22:23:00", "112:42"});
-  std::vector<const char*> expected_str({"1648041780", "1648041780", ""});
+  auto current = std::chrono::time_point_cast<std::chrono::microseconds>(std::chrono::system_clock::now());
+  auto current_tt = std::chrono::system_clock::to_time_t(current);
+  auto tm = gmtime(&current_tt);
+  auto local_tt = std::mktime(tm);
+  char utc[40];
+  char local[40];
+  strftime(utc, 40, "%Y-%m-%dT%H:%M:%SZ", tm);
+  strftime(local, 40, "%Y-%m-%dT%H:%M:%S", tm);
+
+  std::vector<const char*> time_str({utc, local, "112:42"});
+  std::vector<const char*> expected_str({std::to_string(current_tt).c_str(), std::to_string(local_tt).c_str(), ""});
 
   for (auto i = 0; i < time_str.size(); i++) {
     auto res = parse_genesis_time(time_str[i]);
