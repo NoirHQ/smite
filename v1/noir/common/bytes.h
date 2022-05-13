78f464fc561d95b932d1c33aee32248cb3ca4807
Jeeyong Um <conr2d@gmail.com>
2022-05-13 10:54:37 +0800

    refactor(v1): bytes

--- v1/noir/common/bytes.h
+++ v1/noir/common/bytes.h
@@ -1,34 +1,186 @@
-// This file is part of NOIR.
-//
-// Copyright (c) 2022 Haderech Pte. Ltd.
-// SPDX-License-Identifier: AGPL-3.0-or-later
-//
 #pragma once
-#include <noir/common/concepts.h>
-#include <noir/common/string.h>
+#include <noir/common/mem_clr.h>
+#include <cppcodec/hex_lower.hpp>
+#include <array>
+#include <span>
+#include <string>
 #include <vector>
 
 namespace noir {
 
-/// \brief alias name of default byte type
-/// \ingroup common
-using ByteType = char;
+template<size_t N = std::dynamic_extent>
+class Bytes {
+private:
+  template<size_t S>
+  struct Backend {
+    using type = std::array<unsigned char, S>;
+  };
+  template<>
+  struct Backend<std::dynamic_extent> {
+    using type = std::vector<unsigned char>;
+  };
 
-/// \brief alias name for variable-length byte sequence
-/// \ingroup common
-template<Byte T>
-using BytesVec = std::vector<T>;
+  typename Backend<N>::type backend;
 
-/// \brief convenient name for variable-length sequence of default byte type
-/// \ingroup common
-using Bytes = BytesVec<ByteType>;
+public:
+  using raw_type = decltype(backend);
+  using value_type = typename raw_type::value_type;
+  using size_type = typename raw_type::size_type;
+  using difference_type = typename raw_type::difference_type;
+  using reference = typename raw_type::reference;
+  using const_reference = typename raw_type::const_reference;
+  using pointer = typename raw_type::pointer;
+  using const_pointer = typename raw_type::const_pointer;
+  using iterator = typename raw_type::iterator;
+  using const_iterator = typename raw_type::const_iterator;
+  using reverse_iterator = typename raw_type::reverse_iterator;
+  using const_reverse_iterator = typename raw_type::const_reverse_iterator;
 
-/// \brief stringify bytes
-/// \ingroup common
-std::string to_string(const BytesVec<char>& v);
+  Bytes() = default;
 
-/// \brief stringify bytes
-/// \ingroup common
-std::string to_string(const BytesVec<unsigned char>& v);
+  Bytes(std::string_view s, bool canonical = true) {
+    if constexpr (N != std::dynamic_extent) {
+      auto size = cppcodec::hex_lower::decode(backend.data(), backend.size(), s);
+      if (canonical && size != N) {
+        throw std::runtime_error("invalid bytes length");
+      }
+      if (size < N) {
+        std::fill(backend.begin() + size, backend.end(), 0);
+      }
+    } else {
+      auto vec = cppcodec::hex_lower::decode(s);
+      std::swap(backend, vec);
+    }
+  }
+
+  template<typename T, size_t S>
+  Bytes(std::span<T, S> bytes, bool canonical = true) {
+    if constexpr (N != std::dynamic_extent) {
+      auto size = bytes.size();
+      if (canonical && size != N) {
+        throw std::runtime_error("invalid bytes length");
+      }
+      std::copy(bytes.begin(), bytes.end(), backend.begin());
+      if (size < N) {
+        std::fill(backend.begin() + size, backend.end(), 0);
+      }
+    } else {
+      backend = {bytes.begin(), bytes.end()};
+    }
+  }
+
+  Bytes(std::vector<unsigned char>& vec, bool canonical = true): Bytes(std::span(vec), canonical) {}
+
+  Bytes(std::vector<unsigned char>&& vec) requires (N == std::dynamic_extent) {
+    std::swap(backend, vec);
+  }
+
+  Bytes(size_t size) requires (N == std::dynamic_extent)
+    : backend(size) {}
+
+  raw_type& raw() {
+    return backend;
+  }
+  const raw_type& raw() const {
+    return backend;
+  }
+
+  reference at(size_type pos) {
+    return backend.at(pos);
+  }
+  const_reference at(size_type pos) const {
+    return backend.at(pos);
+  }
+  reference operator[](size_type pos) {
+    return backend[pos];
+  }
+  const_reference operator[](size_type pos) const {
+    return backend[pos];
+  }
+
+  reference front() {
+    return backend.front();
+  }
+  const_reference front() const {
+    return backend.front();
+  }
+  reference back() {
+    return backend.back();
+  }
+  const_reference back() const {
+    return backend.back();
+  }
+
+  iterator begin() noexcept {
+    return backend.begin();
+  }
+  const_iterator begin() const noexcept {
+    return backend.begin();
+  }
+  iterator end() noexcept {
+    return backend.end();
+  }
+  const_iterator end() const noexcept {
+    return backend.end();
+  }
+
+  iterator rbegin() noexcept {
+    return backend.rbegin();
+  }
+  const_iterator rbegin() const noexcept {
+    return backend.rbegin();
+  }
+  iterator rend() noexcept {
+    return backend.rend();
+  }
+  const_iterator rend() const noexcept {
+    return backend.rend();
+  }
+
+  pointer data() noexcept {
+    return backend.data();
+  }
+  const_pointer data() const noexcept {
+    return backend.data();
+  }
+  size_type size() const noexcept {
+    return backend.size();
+  }
+  [[nodiscard]] bool empty() const noexcept {
+    if constexpr (!N) {
+      return true;
+    } else if constexpr (N == 1) {
+      return !backend[0];
+    }
+    for (auto i = 0; i < size(); ++i) {
+      if (backend[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+  void resize(size_type size) requires (N == std::dynamic_extent) {
+    backend.resize(size);
+  }
+
+  std::string to_string() const {
+    return cppcodec::hex_lower::encode(backend);
+  }
+
+  void clear() {
+    std::fill(backend.begin(), backend.end(), 0);
+  }
+};
+
+template<size_t N = std::dynamic_extent>
+class SecureBytes : public Bytes<N> {
+private:
+  using super = Bytes<N>;
+
+public:
+  ~SecureBytes() {
+    mem_cleanse(super::data(), super::size());
+  }
+};
 
 } // namespace noir
