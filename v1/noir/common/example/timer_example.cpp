fb444e7448f5f7f720145c4cec6906097ca59135
code0xff <ian.jungyong.um@gmail.com>
2022-05-13 16:54:29 +0800

    test: use milliseconds for default time unit

--- v1/noir/common/example/timer_example.cpp
+++ v1/noir/common/example/timer_example.cpp
@@ -5,34 +5,32 @@
 //
 #include <noir/common/throttle_timer.h>
 #include <boost/asio/io_context.hpp>
-#include <boost/asio/steady_timer.hpp>
 #include <iostream>
 
 using namespace noir;
 
 int main() {
   boost::asio::io_context io_context;
-  Timer timer{io_context, std::chrono::seconds{1}};
+  Timer timer{io_context, std::chrono::milliseconds{1000}};
 
   timer.after_func([&]() -> boost::asio::awaitable<Result<void>> {
     std::cout << "function start" << std::endl;
-    boost::asio::steady_timer st{io_context};
-    st.expires_after(std::chrono::seconds{1});
+    boost::asio::steady_timer dt{io_context};
+    dt.expires_after(std::chrono::milliseconds{1000});
 
-    co_await st.async_wait(boost::asio::use_awaitable);
+    co_await dt.async_wait(boost::asio::use_awaitable);
     std::cout << "function end" << std::endl;
-    timer.reset(std::chrono::seconds{1});
+    timer.reset(std::chrono::milliseconds{1000});
     co_return success();
   });
 
   boost::asio::co_spawn(
     io_context,
     [&]() -> boost::asio::awaitable<void> {
-      boost::asio::steady_timer st{io_context};
-      st.expires_after(std::chrono::seconds{3});
-      co_await st.async_wait(boost::asio::use_awaitable);
+      boost::asio::steady_timer dt{io_context};
+      dt.expires_after(std::chrono::milliseconds{5000});
+      co_await dt.async_wait(boost::asio::use_awaitable);
       timer.stop();
-      std::cout << 1 << std::endl;
     },
     boost::asio::detached);
 
