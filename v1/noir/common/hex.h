3a3cb7aa11f6e4e08aefa7c686c18062e22f9443
Jeeyong Um <conr2d@gmail.com>
2022-05-16 13:46:48 +0800

    fix(v1): add missing hex.h

---
+++ v1/noir/common/hex.h
@@ -0,0 +1,13 @@
+// This file is part of NOIR.
+//
+// Copyright (c) 2022 Haderech Pte. Ltd.
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+#pragma once
+#include <cppcodec/hex_lower.hpp>
+
+namespace noir {
+
+using hex = cppcodec::hex_lower;
+
+} // namespace noir
