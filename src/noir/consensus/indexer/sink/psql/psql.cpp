b9ce1ebd61dcc1f47819bfcf32123a37283d2260
nonvis@gmail.com <nonvis@gmail.com>
2022-06-06 15:49:10 +0800

    refactor(consensus): use Result in place of result for indexer

--- src/noir/consensus/indexer/sink/psql/psql.cpp
+++ src/noir/consensus/indexer/sink/psql/psql.cpp
@@ -3,7 +3,6 @@
 // Copyright (c) 2022 Haderech Pte. Ltd.
 // SPDX-License-Identifier: AGPL-3.0-or-later
 //
-#pragma once
 #include <noir/consensus/indexer/sink/psql/psql.h>
 #include <noir/core/codec.h>
 
@@ -12,12 +11,12 @@
 
 namespace noir::consensus::indexer {
 
-using query_func = std::function<result<void>(pqxx::work&)>;
+using query_func = std::function<Result<void>(pqxx::work&)>;
 
 struct psql_event_sink_impl {
 
-  result<void> index_block_events(const events::event_data_new_block_header& h) {
-    return run_in_transaction([this, &h](pqxx::work& tx) -> result<void> {
+  Result<void> index_block_events(const events::event_data_new_block_header& h) {
+    return run_in_transaction([this, &h](pqxx::work& tx) -> Result<void> {
       auto ts = get_utc_ts();
       std::string query = "INSERT INTO blocks (height, chain_id, created_at) VALUES ($1, $2, $3) ON CONFLICT DO "
                           "NOTHING RETURNING rowid;";
@@ -27,33 +26,32 @@ struct psql_event_sink_impl {
       args.append(ts);
       auto block_id = query_with_id(tx, query, args);
       if (!block_id)
-        return make_unexpected(fmt::format("indexing block header: {}", block_id.error()));
+        return Error::format("indexing block header: {}", block_id.error());
 
       // Insert special block meta-event
       if (auto ok = insert_events(tx, block_id.value(), 0,
             {make_indexed_event(std::string(events::block_height_key), std::to_string(h.header.height))});
           !ok)
-        return make_unexpected(fmt::format("block meta-events: {}", ok.error()));
+        return Error::format("block meta-events: {}", ok.error());
       // Insert all block events
       if (auto ok = insert_events(tx, block_id.value(), 0, h.result_begin_block.events); !ok)
-        return make_unexpected(fmt::format("begin-block events: {}", ok.error()));
+        return Error::format("begin-block events: {}", ok.error());
       if (auto ok = insert_events(tx, block_id.value(), 0, h.result_end_block.events); !ok)
-        return make_unexpected(fmt::format("end-block events: {}", ok.error()));
-      return {};
+        return Error::format("end-block events: {}", ok.error());
     });
   }
 
-  result<void> index_tx_events(const std::vector<tx_result>& txrs) {
+  Result<void> index_tx_events(const std::vector<tx_result>& txrs) {
     auto ts = get_utc_ts();
     for (const auto& txr : txrs) {
       crypto::sha3_256 hash;
       auto tx_hash = hash(txr.tx);
 
-      auto ok = run_in_transaction([this, &txr, &ts, &tx_hash](pqxx::work& tx) -> result<void> {
+      auto ok = run_in_transaction([this, &txr, &ts, &tx_hash](pqxx::work& tx) -> Result<void> {
         auto block_id = query_with_id(
           tx, "SELECT rowid FROM blocks WHERE height = $1 AND chain_id = $2;", {std::to_string(txr.height), chain_id});
         if (!block_id)
-          return make_unexpected(fmt::format("finding block_id: {}", block_id.error()));
+          return Error::format("finding block_id: {}", block_id.error());
 
         // Insert for this tx_result and capture id for indexing events
         std::string query = "INSERT INTO tx_results (block_id, index, created_at, tx_hash, tx_result) ";
@@ -66,29 +64,26 @@ struct psql_event_sink_impl {
         args.append(to_string(encode(txr)));
         auto tx_id = query_with_id(tx, query, args);
         if (!tx_id)
-          return make_unexpected(fmt::format("indexing tx_result: {}", tx_id.error()));
+          return Error::format("indexing tx_result: {}", tx_id.error());
 
         // Insert special transaction meta-events
         if (auto ok = insert_events(tx, block_id.value(), tx_id.value(),
               {make_indexed_event(std::string(events::tx_hash_key), to_hex(tx_hash)),
                 make_indexed_event(std::string(events::tx_height_key), std::to_string(txr.height))});
             !ok)
-          return make_unexpected(fmt::format("indexing transaction meta-events: {}", ok.error()));
+          return Error::format("indexing transaction meta-events: {}", ok.error());
         // Insert events packaged with transaction
         if (auto ok = insert_events(tx, block_id.value(), tx_id.value(), txr.result.events); !ok)
-          return make_unexpected(fmt::format("indexing transaction events: {}", ok.error()));
-        return {};
+          return Error::format("indexing transaction events: {}", ok.error());
       });
     }
-    return {};
   }
 
-  result<void> stop() {
+  Result<void> stop() {
     try {
       C->close();
     } catch (std::exception const& e) {
     }
-    return {};
   }
 
   void setup(const std::string& conn_str, const std::string& new_chain_id) {
@@ -97,17 +92,16 @@ struct psql_event_sink_impl {
   }
 
 private:
-  result<void> run_in_transaction(const query_func& f) {
+  Result<void> run_in_transaction(const query_func& f) {
     pqxx::work tx(*C);
     if (auto ok = f(tx); !ok) {
       tx.abort(); // Not actually needed but makes it more explicit
-      return make_unexpected(ok.error());
+      return ok.error();
     }
     tx.commit();
-    return {};
   }
 
-  result<void> insert_events(pqxx::work& tx, uint32_t block_id, uint32_t tx_id, const std::vector<event>& evts) {
+  Result<void> insert_events(pqxx::work& tx, uint32_t block_id, uint32_t tx_id, const std::vector<event>& evts) {
     for (const auto& evt : evts) {
       if (evt.type.empty())
         continue;
@@ -119,7 +113,7 @@ private:
       args.append(evt.type);
       auto eid = query_with_id(tx, query, args);
       if (!eid)
-        return make_unexpected(eid.error());
+        return eid.error();
 
       // Add any attributes flagged for indexing
       for (const auto& attr : evt.attributes) {
@@ -130,20 +124,19 @@ private:
           tx.exec_params("INSERT INTO attributes (event_id, key, composite_key, value) VALUES ($1, $2, $3, $4)",
             eid.value(), attr.key, composite_key, attr.value);
         } catch (std::exception const& e) {
-          return make_unexpected(e.what());
+          return Error::format("{}", e.what());
         }
       }
     }
-    return {};
   }
 
-  result<uint32_t> query_with_id(pqxx::work& tx, const std::string& query, const pqxx::params& args) {
+  Result<uint32_t> query_with_id(pqxx::work& tx, const std::string& query, const pqxx::params& args) {
     uint32_t id;
     try {
       pqxx::result r = tx.exec_params(query, args);
       id = r[0][0].as<uint32_t>(); // Assume query always returns one rowid
     } catch (std::exception const& e) {
-      return make_unexpected(e.what());
+      return Error::format("{}", e.what());
     }
     return id;
   }
@@ -173,26 +166,26 @@ private:
 
 psql_event_sink::psql_event_sink(): my(new psql_event_sink_impl()) {}
 
-result<std::shared_ptr<event_sink>> psql_event_sink::new_event_sink(
+Result<std::shared_ptr<event_sink>> psql_event_sink::new_event_sink(
   const std::string& conn_str, const std::string& chain_id) {
   auto new_sink = std::make_shared<psql_event_sink>();
   try {
     new_sink->my->setup(conn_str, chain_id);
   } catch (std::exception const& e) {
-    return make_unexpected(fmt::format("unable to create new_event_sink: {}", e.what()));
+    return Error::format("unable to create new_event_sink: {}", e.what());
   }
   return new_sink;
 }
 
-result<void> psql_event_sink::index_block_events(const events::event_data_new_block_header& h) {
+Result<void> psql_event_sink::index_block_events(const events::event_data_new_block_header& h) {
   return my->index_block_events(h);
 }
 
-result<void> psql_event_sink::index_tx_events(const std::vector<tx_result>& txrs) {
+Result<void> psql_event_sink::index_tx_events(const std::vector<tx_result>& txrs) {
   return my->index_tx_events(txrs);
 }
 
-result<void> psql_event_sink::stop() {
+Result<void> psql_event_sink::stop() {
   return my->stop();
 }
 
--- src/noir/consensus/indexer/sink/psql/psql.h
+++ src/noir/consensus/indexer/sink/psql/psql.h
@@ -12,33 +12,33 @@ struct psql_event_sink : public event_sink {
 
   psql_event_sink();
 
-  static result<std::shared_ptr<event_sink>> new_event_sink(const std::string& conn_str, const std::string& chain_id);
+  static Result<std::shared_ptr<event_sink>> new_event_sink(const std::string& conn_str, const std::string& chain_id);
 
-  result<void> index_block_events(const events::event_data_new_block_header& h) override;
+  Result<void> index_block_events(const events::event_data_new_block_header& h) override;
 
-  result<void> index_tx_events(const std::vector<tx_result>& txrs) override;
+  Result<void> index_tx_events(const std::vector<tx_result>& txrs) override;
 
-  result<std::vector<int64_t>> search_block_events(std::string query) override {
-    return make_unexpected("search_block_events is not supported for postgres event_sink");
+  Result<std::vector<int64_t>> search_block_events(std::string query) override {
+    return Error::format("search_block_events is not supported for postgres event_sink");
   }
 
-  result<std::vector<std::shared_ptr<tx_result>>> search_tx_events(std::string query) override {
-    return make_unexpected("search_tx_events is not supported for postgres event_sink");
+  Result<std::vector<std::shared_ptr<tx_result>>> search_tx_events(std::string query) override {
+    return Error::format("search_tx_events is not supported for postgres event_sink");
   }
 
-  result<std::shared_ptr<tx_result>> get_tx_by_hash(bytes hash) override {
-    return make_unexpected("get_tx_by_hash is not supported for postgres event_sink");
+  Result<std::shared_ptr<tx_result>> get_tx_by_hash(bytes hash) override {
+    return Error::format("get_tx_by_hash is not supported for postgres event_sink");
   }
 
-  result<bool> has_block(int64_t height) override {
-    return make_unexpected("has_block is not supported for postgres event_sink");
+  Result<bool> has_block(int64_t height) override {
+    return Error::format("has_block is not supported for postgres event_sink");
   }
 
   event_sink_type type() override {
     return event_sink_type::psql;
   }
 
-  result<void> stop() override;
+  Result<void> stop() override;
 
 private:
   std::shared_ptr<struct psql_event_sink_impl> my;
