d64dac14f0c643e6a5ac4fe997b160b37de6613c
nonvis@gmail.com <nonvis@gmail.com>
2022-06-06 15:14:56 +0800

    refactor(consensus): add more features to core

---
+++ src/noir/core/basic_errors.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <noir/core/error.h>
+
+namespace noir {
+
+extern const Error err_not_implemented;
+extern const Error err_unreachable;
+extern const Error err_eof;
+
+} // namespace noir
