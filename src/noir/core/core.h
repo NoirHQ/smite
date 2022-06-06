a7aef6b10102639cd0f62a32e3cc6313b18d4cf5
nonvis@gmail.com <nonvis@gmail.com>
2022-06-06 15:14:56 +0800

    refactor(consensus): add more features to core

--- src/noir/core/core.h
+++ src/noir/core/core.h
@@ -4,5 +4,18 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 //
 #pragma once
-
+#include <noir/core/as_result.h>
+#include <noir/core/basic.h>
+#include <noir/core/channel.h>
 #include <noir/core/codec.h>
+#include <boost/asio/co_spawn.hpp>
+#include <boost/asio/detached.hpp>
+#include <boost/asio/experimental/awaitable_operators.hpp>
+#include <boost/asio/io_context.hpp>
+
+namespace noir {
+
+namespace asio = boost::asio;
+using namespace asio::experimental::awaitable_operators;
+
+} // namespace noir
---
+++ src/noir/core/test/result_test.cpp
@@ -0,0 +1,46 @@
+// This file is part of NOIR.
+//
+// Copyright (c) 2022 Haderech Pte. Ltd.
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+#include <catch2/catch_all.hpp>
+#include <noir/core/result.h>
+#include <boost/asio/io_context.hpp>
+
+using namespace noir;
+
+TEST_CASE("Result", "[noir][core]") {
+  SECTION("error propagation") {
+    auto foo = []() -> Result<int> { return Error("error"); };
+
+    auto bar = [&]() -> Result<std::string> {
+      if (auto ok = foo(); !ok) {
+        return ok.error();
+      }
+      return "ok";
+    };
+
+    auto res = bar();
+    CHECK(!res);
+    CHECK(res.error().message() == "error");
+  }
+
+  SECTION("co_spawn") {
+    boost::asio::io_context io_context{};
+    auto foo = []() -> boost::asio::awaitable<Result<void>> {
+      throw std::runtime_error("runtime error");
+      co_return success();
+    };
+    boost::asio::co_spawn(io_context, foo(), [](std::exception_ptr eptr, auto r) {
+      CHECK(!r);
+      CHECK(!(bool)r.error());
+      try {
+        std::rethrow_exception(eptr);
+      } catch (const std::exception& e) {
+        CHECK(std::string(e.what()) == "runtime error");
+      }
+    });
+
+    io_context.run();
+  }
+}
