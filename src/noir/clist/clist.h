1d7466d262edcee3c6cd0173dd7a394ee45fc713
nonvis@gmail.com <nonvis@gmail.com>
2022-06-08 18:12:58 +0800

    feat(noir): implement clist using condition variable to handle wait

--- src/noir/clist/clist.h
+++ src/noir/clist/clist.h
@@ -8,6 +8,183 @@
 
 namespace noir {
 
-struct clist {};
+constexpr int clist_max_length{std::numeric_limits<int>::max()};
+
+template<typename T>
+struct c_element;
+template<typename T>
+using e_ptr = std::shared_ptr<c_element<T>>;
+
+template<typename T>
+struct c_element {
+private:
+  std::mutex mtx;
+  std::condition_variable cv_next;
+  e_ptr<T> prev{};
+  e_ptr<T> next{};
+  bool removed{};
+  T value;
+
+public:
+  explicit c_element(T v): value(v) {}
+
+  static e_ptr<T> new_e(T v) {
+    auto ret = std::make_shared<c_element<T>>(v);
+    return ret;
+  }
+
+  /// brief blocks until next is available or got removed; note may return nullptr
+  e_ptr<T> next_wait() {
+    std::unique_lock<std::mutex> lck(mtx);
+    cv_next.wait(lck, [this] { return (next != nullptr || removed); });
+    return next;
+  }
+
+  e_ptr<T> next_wait_with_timeout(std::chrono::milliseconds timeout) {
+    std::unique_lock<std::mutex> lck(mtx);
+    cv_next.wait_for(lck, timeout, [this] { return (next != nullptr || removed); });
+    return next;
+  }
+
+  e_ptr<T> get_next() {
+    std::scoped_lock _(mtx);
+    return next;
+  }
+
+  e_ptr<T> get_prev() {
+    std::scoped_lock _(mtx);
+    return prev;
+  }
+
+  bool get_removed() {
+    std::scoped_lock _(mtx);
+    return removed;
+  }
+
+  void detach_next() {
+    std::scoped_lock _(mtx);
+    check(removed, "detach_next() must be called after remove");
+    next.reset();
+  }
+
+  void detach_prev() {
+    std::scoped_lock _(mtx);
+    check(removed, "detach_prev() must be called after remove");
+    prev.reset();
+  }
+
+  void set_next(e_ptr<T> new_next) {
+    std::scoped_lock _(mtx);
+    auto old_next = next;
+    next = new_next;
+    // if (old_next && !new_next) {
+    // }
+    if (!old_next && new_next) {
+      cv_next.notify_all();
+    }
+  }
+
+  void set_prev(e_ptr<T> new_prev) {
+    std::scoped_lock _(mtx);
+    auto old_prev = prev;
+    prev = new_prev;
+    // if (!old_prev && new_prev) {
+    //   cv_prev.notify_all();
+    // }
+  }
+
+  void set_removed() {
+    std::scoped_lock _(mtx);
+    removed = true;
+    if (!next)
+      cv_next.notify_all();
+  }
+};
+
+template<typename T>
+struct clist {
+private:
+  std::mutex mtx;
+  std::condition_variable cv;
+  e_ptr<T> head{};
+  e_ptr<T> tail{};
+  int len{};
+  int max_len{};
+
+public:
+  clist() {}
+
+  static std::unique_ptr<clist<T>> new_clist(int max_length = clist_max_length) {
+    auto ret = std::make_unique<clist<T>>();
+    ret->max_len = max_length;
+    return ret;
+  }
+
+  int get_len() {
+    std::scoped_lock _(mtx);
+    return len;
+  }
+
+  e_ptr<T> front() {
+    std::scoped_lock _(mtx);
+    return head;
+  }
+
+  /// brief blocks until front is available
+  e_ptr<T> front_wait() {
+    std::unique_lock<std::mutex> lck(mtx);
+    cv.wait(lck, [this] { return head != nullptr; });
+    return head;
+  }
+
+  e_ptr<T> back() {
+    std::scoped_lock _(mtx);
+    return tail;
+  }
+
+  /// brief blocks until back is available
+  e_ptr<T> back_wait() {
+    std::unique_lock<std::mutex> lck(mtx);
+    cv.wait(lck, !tail);
+    return tail;
+  }
+
+  e_ptr<T> push_back(T v) {
+    std::scoped_lock _(mtx);
+    auto e = c_element<T>::new_e(v);
+    check(len < max_len, fmt::format("clist: maximum length reached {}", max_len));
+    if (tail == nullptr) {
+      head = e;
+      tail = e;
+    } else {
+      e->set_prev(tail);
+      tail->set_next(e);
+      tail = e;
+    }
+    if (len == 0)
+      cv.notify_all();
+    len++;
+    return e;
+  }
+
+  void remove(e_ptr<T> e) {
+    std::scoped_lock _(mtx);
+    auto prev = e->get_prev();
+    auto next = e->get_next();
+    check(head && tail, "remove on empty clist");
+    check(prev || head == e, "remove with false head");
+    check(next || tail == e, "remove with false tail");
+    len--;
+    if (!prev)
+      head = next;
+    else
+      prev->set_next(next);
+    if (!next)
+      tail = prev;
+    else
+      next->set_prev(prev);
+    e->set_removed();
+  }
+};
 
 } // namespace noir
--- src/noir/clist/test/clist_test.cpp
+++ src/noir/clist/test/clist_test.cpp
@@ -4,47 +4,140 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 //
 #include <catch2/catch_all.hpp>
-#include <noir/core/core.h>
-
-#include <boost/asio/awaitable.hpp>
-#include <boost/asio/steady_timer.hpp>
-
-#include <chrono>
+#include <noir/clist/clist.h>
+#include <thread>
 
 using namespace noir;
-using namespace boost::asio::experimental::awaitable_operators;
 
-boost::asio::awaitable<void> recv(Chan<Done>& done, Chan<int>& value) {
-  for (;;) {
-    auto res = co_await(
-      done.async_receive(as_result(boost::asio::use_awaitable)) || value.async_receive(boost::asio::use_awaitable));
-    switch (res.index()) {
-    case 0:
-      co_return;
-    case 1:
-      std::cout << std::get<1>(res) << std::endl;
+TEST_CASE("clist: max_length", "[noir][clist]") {
+  int max_length = 1000;
+  auto l = clist<int>::new_clist(max_length);
+  for (auto i = 0; i < max_length; i++)
+    l->push_back(i);
+  CHECK_THROWS_WITH(l->push_back(1), fmt::format("clist: maximum length reached {}", max_length));
+}
+
+TEST_CASE("clist: small", "[noir][clist]") {
+  auto l = clist<int>::new_clist();
+  auto e1 = l->push_back(1);
+  auto e2 = l->push_back(2);
+  auto e3 = l->push_back(3);
+  CHECK(l->get_len() == 3);
+  l->remove(e1);
+  l->remove(e2);
+  l->remove(e3);
+  CHECK(l->get_len() == 0);
+}
+
+TEST_CASE("clist: scan right delete random", "[noir][clist]") {
+  int num_elements = 1000;
+  int num_times = 100;
+  int num_scanners = 10;
+
+  auto l = clist<int>::new_clist();
+  bool stop{};
+
+  std::vector<e_ptr<int>> els(num_elements);
+  for (auto i = 0; i < num_elements; i++) {
+    auto el = l->push_back(i);
+    els[i] = el;
+  }
+
+  std::vector<std::thread> thrds(num_scanners);
+  std::mutex mtx;
+  for (auto i = 0; i < num_scanners; i++) {
+    thrds[i] = std::thread([i, &stop, &l, &mtx]() {
+      e_ptr<int> el{};
+      int restart_counter{}, counter{};
+      for (;;) {
+        if (stop) {
+          std::scoped_lock _(mtx);
+          std::cout << "stopped\n";
+          std::cout << fmt::format("scanner: {} restart_counter: {} counter: {}\n", i, restart_counter, counter);
+          return;
+        }
+        if (!el) {
+          el = l->front_wait();
+          restart_counter++;
+        }
+        el = el->get_next();
+        counter++;
+      }
+    });
+  }
+
+  for (auto i = 0; i < num_times; i++) {
+    int rm_el_idx = rand() % num_elements;
+    auto rm_el = els[rm_el_idx];
+    l->remove(rm_el);
+    auto new_el = l->push_back(1);
+    els[rm_el_idx] = new_el;
+    if (i % 100000 == 0)
+      std::cout << fmt::format("pushed #{}/1000K so far\n", i);
+  }
+
+  stop = true;
+  for (auto& t : thrds)
+    t.join();
+
+  for (auto el = l->front(); el; el = el->get_next())
+    l->remove(el);
+  CHECK(l->get_len() == 0);
+}
+
+TEST_CASE("clist: wait", "[noir][clist]") {
+  auto l = clist<int>::new_clist();
+  l->push_back(1);
+
+  auto el = l->front();
+  l->remove(el);
+  CHECK(l->get_len() == 0);
+
+  el = l->push_back(0);
+  int pushed{};
+  bool done{};
+  auto thrd = std::thread([&]() {
+    for (auto i = 1; i < 100; i++) {
+      l->push_back(i);
+      pushed++;
+      std::this_thread::sleep_for(std::chrono::milliseconds{25});
     }
+    std::this_thread::sleep_for(std::chrono::milliseconds{25});
+    done = true;
+  });
+
+  auto next = el;
+  int seen{};
+  for (;;) {
+    next = next->next_wait_with_timeout(std::chrono::milliseconds{100});
+    if (next)
+      seen++;
+    if (done)
+      break;
   }
+  thrd.join();
+
+  CHECK(pushed == seen);
 }
 
-boost::asio::awaitable<void> send(Chan<Done>& done, Chan<int>& value, boost::asio::steady_timer&& timer) {
-  boost::system::error_code ec;
-  for (auto i = 1; i <= 5; ++i) {
-    co_await value.async_send(ec, i, boost::asio::use_awaitable);
-    timer.expires_after(std::chrono::seconds(1));
-    co_await timer.async_wait(boost::asio::use_awaitable);
-  }
-  done.close();
+TEST_CASE("clist: removed", "[noir][clist]") {
+  auto l = clist<int>::new_clist();
+  auto el1 = l->push_back(1);
+  auto el2 = l->push_back(2);
+  l->remove(el1);
+  CHECK(el1->get_removed());
+  CHECK(!el2->get_removed());
 }
 
-TEST_CASE("clist: channel", "[noir][clist]") {
-  boost::asio::io_context io_context{};
+TEST_CASE("clist: next wait", "[noir][clist]") {
+  auto l = clist<int>::new_clist();
+  auto el1 = l->push_back(1);
+  CHECK(el1->next_wait_with_timeout(std::chrono::milliseconds{1}) == nullptr);
 
-  Chan<Done> done{io_context};
-  Chan<int> value{io_context};
+  auto el2 = l->push_back(2);
+  CHECK(el1->next_wait_with_timeout(std::chrono::milliseconds{1}) != nullptr);
+  CHECK(el2->next_wait_with_timeout(std::chrono::milliseconds{1}) == nullptr);
 
-  boost::asio::co_spawn(io_context, recv(done, value), boost::asio::detached);
-  boost::asio::co_spawn(io_context, send(done, value, boost::asio::steady_timer{io_context}), boost::asio::detached);
-
-  io_context.run();
+  l->remove(el2);
+  CHECK(el2->next_wait_with_timeout(std::chrono::milliseconds{1}) == nullptr);
 }
