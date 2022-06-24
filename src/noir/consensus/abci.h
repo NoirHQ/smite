63ef77b7266cf729c2247f693a0cf5c58a66c6d5
nonvis@gmail.com <nonvis@gmail.com>
2022-06-24 18:46:57 +0800

    refactor(consensus): remove unused options

--- src/noir/consensus/abci.h
+++ src/noir/consensus/abci.h
@@ -25,8 +25,6 @@ public:
     abci_options->add_option("--mode", "Mode of Node: full | validator | seed (not supported)")
       ->check(CLI::IsMember({"full", "validator", "seed"}))
       ->default_val("validator");
-    abci_options->add_option("--do-not-start-node", "Do not start node (debug purposes)");
-    abci_options->add_option("--start-non-validator-node", "Start node in non-validator mode (debug purposes)");
 
     auto bs_options = app_config.add_section("blocksync",
       "######################################################\n"
@@ -61,7 +59,6 @@ public:
         elog("unknown mode: ${mode}", ("mode", mode_str));
       return;
     }
-    do_not_start_node = abci_options->get_option("--do-not-start-node")->as<bool>();
 
     auto bs_options = app_config.get_subcommand("blocksync");
     bool bs_enable = bs_options->get_option("--enable")->as<bool>();
@@ -74,14 +71,6 @@ public:
     config_->base.root_dir = app.home_dir().string();
     config_->consensus.root_dir = config_->base.root_dir;
     config_->priv_validator.root_dir = config_->base.root_dir;
-
-    if (do_not_start_node) {
-      // Do not start node, but create an instance of consensus_reactor so network messages can be processed
-      auto event_bus_ = std::make_shared<events::event_bus>(app);
-      static std::shared_ptr<consensus_reactor> cs_reactor =
-        std::make_shared<consensus_reactor>(app, std::make_shared<consensus_state>(app, event_bus_), event_bus_, false);
-      return;
-    }
 
     node_ = node::new_default_node(app, config_);
 
@@ -100,9 +89,6 @@ public:
   }
 
   void plugin_startup() {
-    if (do_not_start_node)
-      return;
-
     node_->on_start();
   }
 
@@ -113,7 +99,6 @@ public:
 
   std::unique_ptr<node> node_;
   std::string proxy_app;
-  bool do_not_start_node{false};
 };
 
 } // namespace noir::consensus
