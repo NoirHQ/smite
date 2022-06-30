0641bf5890a05beff069d73748cc29f4e455db34
Jeeyong Um <conr2d@gmail.com>
2022-06-30 16:31:48 +0800

    feat(mempool): move priority queue impl to header

--- src/noir/mempool/priority_queue.h
+++ src/noir/mempool/priority_queue.h
@@ -19,12 +19,61 @@ namespace noir::mempool {
 template<typename... Indices>
 class TxPriorityQueue {
 public:
-  auto get_evictable_txs(int64_t priority, int64_t tx_size, int64_t total_size, int64_t cap) -> std::vector<std::shared_ptr<WrappedTx>>;
-  auto num_txs() -> size_t;
-  void remove_tx(const std::shared_ptr<WrappedTx>& tx);
-  void push_tx(const std::shared_ptr<WrappedTx>& tx);
-  void push_tx(std::shared_ptr<WrappedTx>&& tx);
-  auto pop_tx() -> std::shared_ptr<WrappedTx>;
+  auto get_evictable_txs(int64_t priority, int64_t tx_size, int64_t total_size, int64_t cap) -> std::vector<std::shared_ptr<WrappedTx>> {
+    std::shared_lock g{mtx};
+
+    std::vector<std::shared_ptr<WrappedTx>> to_evict;
+    auto curr_size = total_size;
+    auto& heap = txs.template get<TxPriorityQueue::by_priority>();
+    for (auto& tx : heap) {
+      if (tx->priority >= priority) {
+        break;
+      }
+
+      to_evict.push_back(tx);
+      curr_size -= tx->size();
+
+      if ((curr_size + tx_size) <= cap) {
+        return to_evict;
+      }
+    }
+    return {};
+  }
+
+  auto num_txs() -> size_t {
+    std::shared_lock g{mtx};
+    return txs.size();
+  }
+
+  void remove_tx(const std::shared_ptr<WrappedTx>& tx) {
+    std::unique_lock g{mtx};
+    txs.erase(txs.find(tx.get()));
+  }
+
+  void push_tx(const std::shared_ptr<WrappedTx>& tx) {
+    std::unique_lock g{mtx};
+    txs.insert(tx);
+  }
+
+  void push_tx(std::shared_ptr<WrappedTx>&& tx) {
+    std::unique_lock g{mtx};
+    txs.insert(std::move(tx));
+  }
+
+  auto pop_tx() -> std::shared_ptr<WrappedTx> {
+    std::unique_lock g{mtx};
+
+    auto& heap = txs.template get<TxPriorityQueue::by_priority>();
+    if (!heap.size()) {
+      return nullptr;
+    }
+    // erase() doesn't work with reverse iterator
+    auto rbegin = --heap.end();
+    auto _ = make_scope_exit([&]() {
+      heap.erase(rbegin);
+    });
+    return *rbegin;
+  }
 
   // XXX: original implementation includes heap interfaces, but here boost::multi_index handles them
 
